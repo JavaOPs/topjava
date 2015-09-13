a4eea064c3101e333b0803d70b1059534a08dab6
Un_Ami <apalkov.e@gmail.com>
2015-09-13 15:44:55 +0300

    Default

---
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -0,0 +1,24 @@
+package ru.javawebinar.topjava.web;
+
+import ru.javawebinar.topjava.LoggerWrapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * User: gkislin
+ * Date: 19.08.2014
+ */
+public class UserServlet extends HttpServlet {
+    private static final LoggerWrapper LOG = LoggerWrapper.get(UserServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirect to userList");
+
+//        request.getRequestDispatcher("/userList.jsp").forward(request, response);
+        response.sendRedirect("userList.jsp");
+    }
+}
