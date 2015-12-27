048dbeca39d40ad1ae47ed45bfda21f6cfe81dc4
Thetitr <genya_g777@ukr.net>
2015-12-27 17:32:06 +0200

    2_Tomcat

---
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -0,0 +1,19 @@
+package ru.javawebinar.topjava.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * User: gkislin
+ * Date: 19.08.2014
+ */
+public class UserServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        request.getRequestDispatcher("/userList.jsp").forward(request, response);
+        response.sendRedirect("userList.jsp");
+    }
+}
