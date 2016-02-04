d63e367ab59b796d7318050096442277804a46f8
Thetitr <genya_g777@ukr.net>
2016-02-04 21:34:39 +0200

    5.3

---
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -0,0 +1,13 @@
+package ru.javawebinar.topjava;
+
+/**
+ * User: gkislin
+ * Date: 19.08.2014
+ */
+public class Profiles {
+    public static final String
+            POSTGRES = "postgres",
+            HSQLDB = "hsqldb",
+            JDBC = "jdbc",
+            JPA = "jpa";
+}
