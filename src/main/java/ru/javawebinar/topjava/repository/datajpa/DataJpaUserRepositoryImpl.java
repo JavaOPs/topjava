0d1523c699279f45d2531273eadbac674f82123e
Thetitr <genya_g777@ukr.net>
2016-02-11 21:13:34 +0200

    5.5

---
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserRepositoryImpl.java
@@ -0,0 +1,47 @@
+package ru.javawebinar.topjava.repository.datajpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.List;
+
+/**
+ * GKislin
+ * 27.03.2015.
+ */
+
+@Repository
+public class DataJpaUserRepositoryImpl implements UserRepository {
+    private static final Sort SORT_NAME_EMAIL = new Sort("name", "email");
+
+    @Autowired
+    private ProxyUserRepository proxy;
+
+    @Override
+    public User save(User user) {
+        return proxy.save(user);
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return proxy.delete(id) != 0;
+    }
+
+    @Override
+    public User get(int id) {
+        return proxy.findOne(id);
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        return proxy.getByEmail(email);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return proxy.findAll(SORT_NAME_EMAIL);
+    }
+}
---
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/ProxyUserRepository.java
@@ -0,0 +1,37 @@
+package ru.javawebinar.topjava.repository.datajpa;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.User;
+
+import java.util.List;
+
+/**
+ * GKislin
+ * 27.03.2015.
+ */
+@Transactional(readOnly = true)
+public interface ProxyUserRepository extends JpaRepository<User, Integer> {
+
+    @Transactional
+    @Modifying
+//    @Query(name = User.DELETE)
+    @Query("DELETE FROM User u WHERE u.id=:id")
+    int delete(@Param("id") int id);
+
+    @Override
+    @Transactional
+    User save(User user);
+
+    @Override
+    User findOne(Integer id);
+
+    @Override
+    List<User> findAll(Sort sort);
+
+    User getByEmail(String email);
+}
