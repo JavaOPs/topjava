f330b35800f70e74ea207640368f945c76771a8b
Thetitr <genya_g777@ukr.net>
2016-02-11 23:01:38 +0200

    6.1

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserMealRepositoryImpl.java
@@ -1,6 +1,10 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.repository.UserMealRepository;
 
@@ -12,29 +16,47 @@ import java.util.List;
  * 27.03.2015.
  */
 @Repository
-public class DataJpaUserMealRepositoryImpl implements UserMealRepository{
-    @Override
-    public UserMeal save(UserMeal userMeal, int userId) {
-        return null;
+public class DataJpaUserMealRepositoryImpl implements UserMealRepository {
+    @Autowired
+    private ProxyUserMealRepository proxy;
+
+    @Autowired
+    private ProxyUserRepository userProxy;
+
+
+
+        @Override
+        @Transactional
+        public UserMeal save(UserMeal userMeal, int userId) {
+            if (!userMeal.isNew() && get(userMeal.getId(), userId) == null) {
+                return null;
+            }
+            userMeal.setUser(userProxy.getOne(userId));
+            return proxy.save(userMeal);
+
+        }
+
+        @Override
+        public boolean delete(int id, int userId) {
+            return proxy.delete(id, userId) != 0;
+        }
+
+        @Override
+        public UserMeal get(int id, int userId) {
+            return proxy.get(id, userId);
+        }
+
+        @Override
+        public List<UserMeal> getAll(int userId) {
+            return proxy.getAll(userId);
+        }
+
+        @Override
+        public List<UserMeal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
+            return proxy.getBetween(startDate, endDate, userId);
+
+        }
+
+
     }
 
-    @Override
-    public boolean delete(int id, int userId) {
-        return false;
-    }
-
-    @Override
-    public UserMeal get(int id, int userId) {
-        return null;
-    }
-
-    @Override
-    public List<UserMeal> getAll(int userId) {
-        return null;
-    }
-
-    @Override
-    public List<UserMeal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
-    }
-}
