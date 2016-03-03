62a67135933dec7fd027e2008a3747ffb23df683
boklag <e.boklag@gms-worldwide.com>
2016-03-03 15:05:42 +0200

    8.2

---
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -0,0 +1,24 @@
+package ru.javawebinar.topjava.web;
+
+import org.junit.Test;
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * GKislin
+ * 10.04.2015.
+ */
+public class ResourceControllerTest extends AbstractControllerTest {
+
+    @Test
+    public void testResources() throws Exception {
+        mockMvc.perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(content().contentType(MediaType.valueOf("text/css")))
+                .andExpect(status().isOk());
+    }
+}
