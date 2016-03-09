bfbe7ac935e18e8c9639e3d90c98728ffde9dfbc
Dmitry <devernul@gmail.com>
2016-03-09 23:39:35 +0600

    HW1-(6)

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.web;
 
+import ru.javawebinar.topjava.LoggerWrapper;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,8 +13,11 @@ import java.io.IOException;
  * Date: 19.08.2014
  */
 public class UserServlet extends HttpServlet {
+    private static final LoggerWrapper LOG = LoggerWrapper.get(UserServlet.class);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirect to userList");
+
 //        request.getRequestDispatcher("/userList.jsp").forward(request, response);
         response.sendRedirect("userList.jsp");
     }
