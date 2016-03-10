34bb8c3a15521cb07218ace160b187dfacd709bd
Dmitriy Varygin <dmitriy.varygin@yandex.ru>
2016-03-10 11:47:17 +0500

    Applied 2-3 patch

---
+++ src/main/java/ru/javawebinar/topjava/model/NamedEntity.java
@@ -0,0 +1,31 @@
+package ru.javawebinar.topjava.model;
+
+/**
+ * User: gkislin
+ * Date: 22.08.2014
+ */
+public class NamedEntity extends BaseEntity {
+
+    protected String name;
+
+    public NamedEntity() {
+    }
+
+    protected NamedEntity(Integer id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
