6a1e1e425677078159650bb5e3767200ac1400f5
VladimirSentcovForJavarushTestTask <vladimirsencov@gmail.ru>
2016-03-10 16:58:33 +0300

    Apply 2-4 patch

---
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -0,0 +1,23 @@
+package ru.javawebinar.topjava;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.Arrays;
+
+/**
+ * User: gkislin
+ * Date: 22.08.2014
+ */
+public class SpringMain {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        System.out.println(Arrays.toString(appCtx.getBeanDefinitionNames()));
+
+//        UserRepository userRepository = (UserRepository) appCtx.getBean("mockUserRepository");
+        UserRepository userRepository = appCtx.getBean(UserRepository.class);
+        userRepository.getAll();
+        appCtx.close();
+    }
+}
