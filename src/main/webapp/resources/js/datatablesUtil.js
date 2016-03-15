ef086b2992f43815412e34b851027cf86e9f699c
boklag <e.boklag@gms-worldwide.com>
2016-03-15 17:55:58 +0200

    8.8

---
+++ src/main/webapp/resources/js/datatablesUtil.js
@@ -0,0 +1,50 @@
+function makeEditable() {
+
+    $('#add').click(function () {
+        $('#id').val(0);
+        $('#editRow').modal();
+    });
+
+    $('.delete').click(function () {
+        deleteRow($(this).attr("id"));
+    });
+
+    $('#detailsForm').submit(function () {
+        save();
+        return false;
+    });
+}
+
+function deleteRow(id) {
+    $.ajax({
+        url: ajaxUrl + id,
+        type: 'DELETE',
+        success: function () {
+            updateTable();
+        }
+    });
+}
+
+function updateTable() {
+    $.get(ajaxUrl, function (data) {
+        oTable_datatable.fnClearTable();
+        $.each(data, function (key, item) {
+            oTable_datatable.fnAddData(item);
+        });
+        oTable_datatable.fnDraw();
+    });
+}
+
+function save() {
+    var form = $('#detailsForm');
+    debugger;
+    $.ajax({
+        type: "POST",
+        url: ajaxUrl,
+        data: form.serialize(),
+        success: function () {
+            $('#editRow').modal('hide');
+            updateTable();
+        }
+    });
+}
