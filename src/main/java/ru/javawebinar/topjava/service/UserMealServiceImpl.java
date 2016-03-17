4d01c97b76f9420aebe064af596c0307edc0944c
Dmitriy Varygin <dmitriy.varygin@yandex.ru>
2016-03-17 10:14:30 +0500

    Applied patch 3-02

--- src/main/java/ru/javawebinar/topjava/service/UserMealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/UserMealServiceImpl.java
@@ -1,13 +1,51 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.repository.UserMealRepository;
+import ru.javawebinar.topjava.util.exception.ExceptionUtil;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
 
 /**
  * GKislin
  * 06.03.2015.
  */
+@Service
 public class UserMealServiceImpl implements UserMealService {
 
+    @Autowired
     private UserMealRepository repository;
 
+    @Override
+    public UserMeal get(int id, int userId) {
+        return ExceptionUtil.check(repository.get(id, userId), id);
+    }
+
+    @Override
+    public void delete(int id, int userId) {
+        ExceptionUtil.check(repository.delete(id, userId), id);
+    }
+
+    @Override
+    public Collection<UserMeal> getBetweenDateTimes(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
+        return repository.getBetween(startDateTime, endDateTime, userId);
+    }
+
+    @Override
+    public Collection<UserMeal> getAll(int userId) {
+        return repository.getAll(userId);
+    }
+
+    @Override
+    public UserMeal update(UserMeal meal, int userId) {
+        return ExceptionUtil.check(repository.save(meal, userId), meal.getId());
+    }
+
+    @Override
+    public UserMeal save(UserMeal meal, int userId) {
+        return repository.save(meal, userId);
+    }
 }
--- src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.util;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -9,6 +10,8 @@ import java.time.format.DateTimeFormatter;
  */
 public class TimeUtil {
     public static final DateTimeFormatter DATE_TME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    public static final LocalDate MIN_DATE = LocalDate.of(0, 1, 1);
+    public static final LocalDate MAX_DATE = LocalDate.of(3000, 1, 1);
 
     public static <T extends Comparable<? super T>> boolean isBetween(T value, T start, T end) {
         return value.compareTo(start) >= 0 && value.compareTo(end) <= 0;
