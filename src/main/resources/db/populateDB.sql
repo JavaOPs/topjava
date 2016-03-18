b0a87d63e7556fb67d8522c62e6a1c66300d2f40
Dmitriy Varygin <dmitriy.varygin@yandex.ru>
2016-03-18 22:25:12 +0500

    Added population meals

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -11,3 +11,8 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id) VALUES
+  (now(), 'Ужин', 600, 100000),
+  (now(), 'Завтрак', 500, 100000),
+  (now(), 'Обед', 800, 100001);
