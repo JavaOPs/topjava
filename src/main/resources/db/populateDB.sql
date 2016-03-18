367b08243c6e5f06ceaa3dbdb04fb3438cdba359
Dmitriy Varygin <dmitriy.varygin@yandex.ru>
2016-03-18 22:32:27 +0500

    Changed timestamp in populating table meals

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -13,6 +13,6 @@ INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_ADMIN', 100001);
 
 INSERT INTO meals (date_time, description, calories, user_id) VALUES
-  (now(), 'Ужин', 600, 100000),
-  (now(), 'Завтрак', 500, 100000),
-  (now(), 'Обед', 800, 100001);
+  ('2015-05-05 04:05:06', 'Ужин', 600, 100000),
+  ('2010-05-31 22:00:00', 'Завтрак', 500, 100000),
+  ('2015-12-12 12:00:00', 'Обед', 800, 100001);
