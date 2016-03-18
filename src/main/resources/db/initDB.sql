d8d8d11bb49516386cca2c5f3f478371d1900089
Dmitriy Varygin <dmitriy.varygin@yandex.ru>
2016-03-18 22:20:38 +0500

    Added autogeneration meal's id with help sequence

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -27,7 +27,7 @@ CREATE TABLE user_roles
 
 CREATE TABLE meals
 (
-  id             INTEGER PRIMARY KEY NOT NULL,
+  id             INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
   date_time      TIMESTAMP NOT NULL,
   description    VARCHAR NOT NULL,
   calories       INTEGER NOT NULL,
