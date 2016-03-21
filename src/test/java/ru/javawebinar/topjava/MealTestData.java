062eb4f8fe2a75c9692d42e4919c715b15de4b81
Dmitriy Varygin <dmitriy.varygin@yandex.ru>
2016-03-21 15:46:52 +0500

    Changed test data for better testing

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -33,17 +33,17 @@ public class MealTestData {
     public static final UserMeal MEAL2 = new UserMeal(ID2, LocalDateTime.of(2010, 5, 31, 22, 0, 0), "Ужин", 500);
 
     public static final UserMeal MEAL3 = new UserMeal(ID3, LocalDateTime.of(2008, 10, 25, 10, 0, 0), "Завтрак", 600);
-    public static final UserMeal MEAL4 = new UserMeal(ID4, LocalDateTime.of(2015, 11, 25, 15, 0, 0), "Обед", 800);
+    public static final UserMeal MEAL4 = new UserMeal(ID4, LocalDateTime.of(2011, 1, 1, 10, 0, 0), "Завтрак", 500);
     public static final UserMeal MEAL5 = new UserMeal(ID5, LocalDateTime.of(2011, 1, 1, 14, 0, 0), "Плотный обед", 1600);
-    public static final UserMeal MEAL6 = new UserMeal(ID6, LocalDateTime.of(2011, 1, 1, 10, 0, 0), "Завтрак", 500);
+    public static final UserMeal MEAL6 = new UserMeal(ID6, LocalDateTime.of(2015, 11, 25, 15, 0, 0), "Обед", 800);
 
     public static final UserMeal MEAL7_FOR_SAVE = new UserMeal(LocalDateTime.of(2004, 12, 12, 12, 0, 0), "Обед", 700);
 
     public static final UserMeal MEAL4_UPDATED = new UserMeal(ID4, LocalDateTime.of(2003, 12, 12, 9, 0, 0), "Завтрак", 700);
 
     public static final List<UserMeal> ALL_OF_USER_ID = Arrays.asList(MEAL1, MEAL2);
-    public static final List<UserMeal> BETWEEN_OF_ADMIN_ID = Arrays.asList(MEAL4, MEAL5, MEAL6);
-    public static final List<UserMeal> ALL_OF_ADMIN_ID_AFTER_SAVE_MEAL7 = Arrays.asList(MEAL4, MEAL5, MEAL6, MEAL3,
+    public static final List<UserMeal> BETWEEN_OF_ADMIN_ID = Arrays.asList(MEAL6, MEAL5, MEAL4);
+    public static final List<UserMeal> ALL_OF_ADMIN_ID_AFTER_SAVE_MEAL7 = Arrays.asList(MEAL6, MEAL5, MEAL4, MEAL3,
             MEAL7_FOR_SAVE);
 
 }
