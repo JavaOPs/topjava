1d6a1bee37f3565f5f373e42033469b3aa1089c4
Grigory Kislin <javawebinar@yandex.ru>
2016-03-31 13:52:01 +0300

    5-4-spring-data-jpa

---
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserMealRepositoryImpl.java
@@ -0,0 +1,40 @@
+package ru.javawebinar.topjava.repository.datajpa;
+
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.repository.UserMealRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * GKislin
+ * 27.03.2015.
+ */
+@Repository
+public class DataJpaUserMealRepositoryImpl implements UserMealRepository{
+    @Override
+    public UserMeal save(UserMeal userMeal, int userId) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(int id, int userId) {
+        return false;
+    }
+
+    @Override
+    public UserMeal get(int id, int userId) {
+        return null;
+    }
+
+    @Override
+    public List<UserMeal> getAll(int userId) {
+        return null;
+    }
+
+    @Override
+    public List<UserMeal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
+        return null;
+    }
+}
