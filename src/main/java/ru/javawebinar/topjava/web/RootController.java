71307dff86d68a947f22ca46bc994b9e14e28705
Grigory Kislin <javawebinar@yandex.ru>
2016-04-07 14:47:56 +0300

    6-10-spring-webmvc

---
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -0,0 +1,39 @@
+package ru.javawebinar.topjava.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ru.javawebinar.topjava.LoggedUser;
+import ru.javawebinar.topjava.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * User: gkislin
+ * Date: 22.08.2014
+ */
+@Controller
+public class RootController {
+    @Autowired
+    private UserService service;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String root() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public String userList(Model model) {
+        model.addAttribute("userList", service.getAll());
+        return "userList";
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    public String setUser(HttpServletRequest request) {
+        int userId = Integer.valueOf(request.getParameter("userId"));
+        LoggedUser.setId(userId);
+        return "redirect:meals";
+    }
+}
