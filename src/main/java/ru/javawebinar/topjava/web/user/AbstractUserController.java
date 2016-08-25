897a3f9fedd52e4d01e53daf11f643b4c4ee3a51
snoop187dog <snoop187dog@mail.ru>
2016-08-26 01:13:58 +0300

    Накатил 2_6_add_annotation_processing.patch

--- src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.web.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.javawebinar.topjava.model.User;
@@ -13,6 +14,7 @@ import java.util.List;
 public abstract class AbstractUserController {
     protected final Logger LOG = LoggerFactory.getLogger(getClass());
 
+    @Autowired
     private UserService service;
 
     public List<User> getAll() {
