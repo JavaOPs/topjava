570d916d2cf5f5c56f90db31b4f3bb3663dfef68
snoop187dog <snoop187dog@mail.ru>
2016-08-26 17:52:40 +0300

    Накатил 3_06_bean_life_cycle.patch

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -1,5 +1,8 @@
 package ru.javawebinar.topjava.model;
 
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import java.time.LocalDateTime;
 
 /**
