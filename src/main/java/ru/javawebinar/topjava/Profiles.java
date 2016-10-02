0563ba109ba8ed9cc7f71c3648e7b8160b2ebe47
snoop187dog <snoop187dog@mail.ru>
2016-10-02 19:58:48 +0200

    Накатил 6_02_HW5_profiles.patch

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -9,7 +9,9 @@ public class Profiles {
             POSTGRES = "postgres",
             HSQLDB = "hsqldb",
             JDBC = "jdbc",
-            JPA = "jpa";
+            JPA = "jpa",
+            DATAJPA = "datajpa";
 
     public static final String ACTIVE_DB = POSTGRES;
+    public static final String DB_IMPLEMENTATION = DATAJPA;
 }
