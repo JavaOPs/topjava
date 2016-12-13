d85148b3930f2cc6fe75395046351b80f69166e9
gitSRS <streltsovrs@gmail.com>
2016-12-13 23:17:27 +0200

    Prepare to HW01

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,36 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Created by User on 13.12.2016.
+ */
+public class MealServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(MealServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("forward to meals");
+
+        List<MealWithExceed> mealsWithExceeded = MealsUtil.getAllWithExceeded(MealsUtil.meals, 2000);
+        mealsWithExceeded.forEach(e-> LOG.debug(e.toString()));
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+        //response.sendRedirect("meals.jsp");
+    }
+
+}
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Meals list</title>
+</head>
+<body>
+<h2><a href="index.html">Home</a></h2>
+<h2>Meals list</h2>
+</body>
+</html>
