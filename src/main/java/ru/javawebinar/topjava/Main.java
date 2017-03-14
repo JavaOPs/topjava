7c576d06c25a79a8ea2f93ab8610b9f155b00206
Olga <silverdeerg@gmail.com>
2017-03-15 00:27:51 +0300

    Add ,

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -9,6 +9,6 @@ package ru.javawebinar.topjava;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.format("Hello Topjava Enterprise!");
+        System.out.format("Hello, Topjava Enterprise!");
     }
 }
