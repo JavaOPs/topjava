ebe1bb7d36800d3810d87d3ba1f57f5c21223589
Dukuu <alezin7@gmail.com>
2017-03-17 14:10:12 +0300

    Prepare to HW0

---
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -0,0 +1,34 @@
+package ru.javawebinar.topjava.model;
+
+import java.time.LocalDateTime;
+
+/**
+ * GKislin
+ * 11.01.2015.
+ */
+public class UserMeal {
+    public static int x = 1;
+    private final LocalDateTime dateTime;
+
+    private final String description;
+
+    private final int calories;
+
+    public UserMeal(LocalDateTime dateTime, String description, int calories) {
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+}
