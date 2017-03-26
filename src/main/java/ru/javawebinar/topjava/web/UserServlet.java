b8430332988de589efd509d8731ae85d56ab4e8f
elusive935 <alenka.fml@mail.ru>
2017-03-26 22:09:39 +0300

    1_6_logging

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,10 +1,15 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
 
 /**
  * User: gkislin
@@ -12,7 +17,11 @@ import java.io.IOException;
  */
 public class UserServlet extends HttpServlet {
 
+    private static final Logger LOG = getLogger(UserServlet.class);
+    
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirect to users");
+
 //        request.getRequestDispatcher("/users.jsp").forward(request, response);
         response.sendRedirect("users.jsp");
     }
