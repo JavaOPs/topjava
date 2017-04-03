c7027c4d006e31f3ddc94e97d6a87845be4b4692
Dukuu <alezin7@gmail.com>
2017-04-03 20:50:07 +0300

    2 6 annotation processing

--- src/main/java/ru/javawebinar/topjava/repository/mock/MockUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/MockUserRepositoryImpl.java
@@ -2,12 +2,14 @@ package ru.javawebinar.topjava.repository.mock;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import java.util.Collections;
 import java.util.List;
 
+@Repository
 public class MockUserRepositoryImpl implements UserRepository {
     private static final Logger LOG = LoggerFactory.getLogger(MockUserRepositoryImpl.class);
 
