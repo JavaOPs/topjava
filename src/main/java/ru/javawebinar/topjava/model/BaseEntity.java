802cbe67c35a4500711109d3a933f042ad1e7eec
Dukuu <alezin7@gmail.com>
2017-04-03 11:47:11 +0300

    2 3 app layers

---
+++ src/main/java/ru/javawebinar/topjava/model/BaseEntity.java
@@ -0,0 +1,24 @@
+package ru.javawebinar.topjava.model;
+
+public class BaseEntity {
+    protected Integer id;
+
+    public BaseEntity() {
+    }
+
+    protected BaseEntity(Integer id) {
+        this.id = id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public boolean isNew() {
+        return (this.id == null);
+    }
+}
---
+++ src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
@@ -0,0 +1,49 @@
+package ru.javawebinar.topjava.web.user;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.service.UserService;
+
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+
+public abstract class AbstractUserController {
+    protected final Logger LOG = LoggerFactory.getLogger(getClass());
+
+    private UserService service;
+
+    public List<User> getAll() {
+        LOG.info("getAll");
+        return service.getAll();
+    }
+
+    public User get(int id) {
+        LOG.info("get " + id);
+        return service.get(id);
+    }
+
+    public User create(User user) {
+        LOG.info("create " + user);
+        checkNew(user);
+        return service.save(user);
+    }
+
+    public void delete(int id) {
+        LOG.info("delete " + id);
+        service.delete(id);
+    }
+
+    public void update(User user, int id) {
+        LOG.info("update " + user);
+        checkIdConsistent(user, id);
+        service.update(user);
+    }
+
+    public User getByMail(String email) {
+        LOG.info("getByEmail " + email);
+        return service.getByEmail(email);
+    }
+}
