e2c9fab5dc5ae9d832d01aebfccd3062ac86c1db
MorozovAI <m407345@mail.ru>
2017-05-03 18:34:14 +0300

    HW3

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START 100000;
@@ -27,7 +28,7 @@ CREATE TABLE user_roles
 
 CREATE TABLE meals
 (
-  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
   user_id     INTEGER   NOT NULL,
   datetime    TIMESTAMP NOT NULL,
   description TEXT,
