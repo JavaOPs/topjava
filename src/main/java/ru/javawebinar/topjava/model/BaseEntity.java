216bcb995a4c58cd7ac52670d1d02b2be3ad35e2
MorozovAI <m407345@mail.ru>
2017-05-12 20:34:34 +0300

    5 1 HW4

--- src/main/java/ru/javawebinar/topjava/model/BaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/BaseEntity.java
@@ -1,7 +1,13 @@
 package ru.javawebinar.topjava.model;
 
+import org.hibernate.Hibernate;
+
 import javax.persistence.*;
 
+/**
+ * Do not manipulate new (transient) entries in HashSet/HashMap without overriding hashCode
+ * http://stackoverflow.com/questions/5031614
+ */
 @MappedSuperclass
 @Access(AccessType.FIELD)
 public class BaseEntity {
@@ -10,7 +16,9 @@ public class BaseEntity {
     @Id
     @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
-    protected Integer id;
+    // PROPERTY access for id due to bug: https://hibernate.atlassian.net/browse/HHH-3718
+    @Access(value = AccessType.PROPERTY)
+    private Integer id;
 
     protected BaseEntity() {
     }
@@ -28,7 +36,7 @@ public class BaseEntity {
     }
 
     public boolean isNew() {
-        return (this.id == null);
+        return (getId() == null);
     }
 
     @Override
@@ -36,20 +44,20 @@ public class BaseEntity {
         if (this == o) {
             return true;
         }
-        if (o == null || getClass() != o.getClass()) {
+        if (o == null || !getClass().equals(Hibernate.getClass(o))) {
             return false;
         }
         BaseEntity that = (BaseEntity) o;
-        return id != null && id.equals(that.id);
+        return getId() != null && getId().equals(that.getId());
     }
 
     @Override
     public int hashCode() {
-        return (id == null) ? 0 : id;
+        return (getId() == null) ? 0 : getId();
     }
 
     @Override
     public String toString() {
-        return String.format("Entity %s (%s)", getClass().getName(), id);
+        return String.format("Entity %s (%s)", getClass().getName(), getId());
     }
 }
