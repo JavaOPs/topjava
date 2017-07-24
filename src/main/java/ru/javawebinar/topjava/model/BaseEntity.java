5d34e6cb4c7b594a87f43ab64afe92bb56568cb9
Kirill Liutikov <kirill.liutikov@gmail.com>
2017-07-24 23:02:32 +0300

    2_3 app_layers

---
+++ src/main/java/ru/javawebinar/topjava/model/BaseEntity.java
@@ -0,0 +1,29 @@
+package ru.javawebinar.topjava.model;
+
+public class BaseEntity {
+    protected Integer id;
+
+    public BaseEntity() {
+    }
+
+    protected BaseEntity(Integer id) {
+        this.id = id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public boolean isNew() {
+        return (this.id == null);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Entity %s (%s)", getClass().getName(), getId());
+    }
+}
---
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminRestController.java
@@ -0,0 +1,38 @@
+package ru.javawebinar.topjava.web.user;
+
+import ru.javawebinar.topjava.model.User;
+
+import java.util.List;
+
+public class AdminRestController extends AbstractUserController {
+
+    @Override
+    public List<User> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    public User get(int id) {
+        return super.get(id);
+    }
+
+    @Override
+    public User create(User user) {
+        return super.create(user);
+    }
+
+    @Override
+    public void delete(int id) {
+        super.delete(id);
+    }
+
+    @Override
+    public void update(User user, int id) {
+        super.update(user, id);
+    }
+
+    @Override
+    public User getByMail(String email) {
+        return super.getByMail(email);
+    }
+}
