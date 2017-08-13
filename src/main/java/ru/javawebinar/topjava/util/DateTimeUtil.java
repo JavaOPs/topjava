68ce086fcb96d04204192a9c020544f776f9686d
Kirill Liutikov <kirill.liutikov@gmail.com>
2017-08-13 19:16:23 +0300

    3_04_HW2_optional_filter

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -1,7 +1,10 @@
 package ru.javawebinar.topjava.util;
 
+import org.springframework.util.StringUtils;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
@@ -16,5 +19,13 @@ public class DateTimeUtil {
 
     public static String toString(LocalDateTime ldt) {
         return ldt == null ? "" : ldt.format(DATE_TIME_FORMATTER);
+    }
+
+    public static LocalDate parseLocalDate(String str) {
+        return StringUtils.isEmpty(str) ? null : LocalDate.parse(str);
+    }
+
+    public static LocalTime parseLocalTime(String str) {
+        return StringUtils.isEmpty(str) ? null : LocalTime.parse(str);
     }
 }
