5f093f2c24eb4510a9a90afa61fb9a8e9a056346
Kirill Liutikov <kirill.liutikov@gmail.com>
2017-08-16 18:28:09 +0300

    3_13_fix_servlet

--- src/main/java/ru/javawebinar/topjava/AuthorizedUser.java
+++ src/main/java/ru/javawebinar/topjava/AuthorizedUser.java
@@ -1,9 +1,11 @@
 package ru.javawebinar.topjava;
 
+import ru.javawebinar.topjava.model.BaseEntity;
+
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class AuthorizedUser {
-    private static int id = 1;
+    private static int id = BaseEntity.START_SEQ;
 
     public static int id() {
         return id;
--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -28,7 +28,7 @@ public class MealServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml");
         mealController = springContext.getBean(MealRestController.class);
     }
 
