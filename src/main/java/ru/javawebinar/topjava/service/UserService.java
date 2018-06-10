4d8cf514f4fdace0a1370a01132e4307d3d4bf2a
Roman <123698745bob@gmail.com>
2018-06-10 21:49:00 +0300

    2 3 app layers

---
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -0,0 +1,22 @@
+package ru.javawebinar.topjava.service;
+
+
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.util.List;
+
+public interface UserService {
+
+    User create(User user);
+
+    void delete(int id) throws NotFoundException;
+
+    User get(int id) throws NotFoundException;
+
+    User getByEmail(String email) throws NotFoundException;
+
+    void update(User user);
+
+    List<User> getAll();
+}
