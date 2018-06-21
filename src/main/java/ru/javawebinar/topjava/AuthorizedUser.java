117e66e039fc277ccafdcfb26df1e5a08e73a193
Roman <123698745bob@gmail.com>
2018-06-21 11:00:10 +0300

    3 13 fix servlet

--- src/main/java/ru/javawebinar/topjava/AuthorizedUser.java
+++ src/main/java/ru/javawebinar/topjava/AuthorizedUser.java
@@ -1,9 +1,11 @@
 package ru.javawebinar.topjava;
 
+import ru.javawebinar.topjava.model.AbstractBaseEntity;
+
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class AuthorizedUser {
-    private static int id = 1;
+    private static int id = AbstractBaseEntity.START_SEQ;
 
     public static int id() {
         return id;
