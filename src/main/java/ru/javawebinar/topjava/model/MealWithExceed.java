7764b493f4c3948a1e952b31956eb33a06f870b5
tiffany <olga.kornilova@gmail.com>
2018-08-12 15:10:51 +0300

    1 0 fix

--- src/main/java/ru/javawebinar/topjava/model/MealWithExceed.java
+++ src/main/java/ru/javawebinar/topjava/model/MealWithExceed.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
  * GKislin
  * 11.01.2015.
  */
-public class UserMealWithExceed {
+public class MealWithExceed {
     private final LocalDateTime dateTime;
 
     private final String description;
@@ -15,7 +15,7 @@ public class UserMealWithExceed {
 
     private final boolean exceed;
 
-    public UserMealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed) {
+    public MealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
