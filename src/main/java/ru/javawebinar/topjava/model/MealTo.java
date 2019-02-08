48ab199fedea17dba952d44370aa10fcdbb9546a
IHOR PETROV <Basicprof@gmail.com>
2019-02-08 12:23:37 +0200

    Рефакторинг проэкта Apply 1_0_fix_patch

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -2,19 +2,19 @@ package ru.javawebinar.topjava.model;
 
 import java.time.LocalDateTime;
 
-public class UserMealWithExceed {
+public class MealTo {
     private final LocalDateTime dateTime;
 
     private final String description;
 
     private final int calories;
 
-    private final boolean exceed;
+    private final boolean excess;
 
-    public UserMealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed) {
+    public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
-        this.exceed = exceed;
+        this.excess = excess;
     }
 }
