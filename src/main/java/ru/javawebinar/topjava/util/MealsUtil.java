54c13ae93e0823a77408998846f9f9cbdd16c2f1
sergo7777777 <sierghieigavrilov053@gmail.com>
2019-03-06 16:32:09 +0200

    1 0 fix

---
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -0,0 +1,31 @@
+package ru.javawebinar.topjava.util;
+
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealTo;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
+public class MealsUtil {
+    public static void main(String[] args) {
+        List<Meal> mealList = Arrays.asList(
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 1000),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 1000),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
+        );
+        getFilteredWithExcess(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+//        .toLocalDate();
+//        .toLocalTime();
+    }
+
+    public static List<MealTo>  getFilteredWithExcess(List<Meal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        // TODO return filtered list with correctly excess field
+        return null;
+    }
+}
