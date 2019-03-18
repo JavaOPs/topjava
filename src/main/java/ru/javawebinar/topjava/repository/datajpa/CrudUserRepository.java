c3ba607ff2a0a21bd5b340f5e44ed5370d0c5bbc
IHOR PETROV <Basicprof@gmail.com>
2019-03-18 20:48:47 +0200

    5 7 spring data jpa

---
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudUserRepository.java
@@ -0,0 +1,33 @@
+package ru.javawebinar.topjava.repository.datajpa;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional(readOnly = true)
+public interface CrudUserRepository extends JpaRepository<User, Integer> {
+    @Transactional
+    @Modifying
+//    @Query(name = User.DELETE)
+    @Query("DELETE FROM User u WHERE u.id=:id")
+    int delete(@Param("id") int id);
+
+    @Override
+    @Transactional
+    User save(User user);
+
+    @Override
+    Optional<User> findById(Integer id);
+
+    @Override
+    List<User> findAll(Sort sort);
+
+    User getByEmail(String email);
+}
---
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserRepositoryImpl.java
@@ -0,0 +1,42 @@
+package ru.javawebinar.topjava.repository.datajpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.List;
+
+@Repository
+public class DataJpaUserRepositoryImpl implements UserRepository {
+    private static final Sort SORT_NAME_EMAIL = new Sort(Sort.Direction.ASC, "name", "email");
+
+    @Autowired
+    private CrudUserRepository crudRepository;
+
+    @Override
+    public User save(User user) {
+        return crudRepository.save(user);
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return crudRepository.delete(id) != 0;
+    }
+
+    @Override
+    public User get(int id) {
+        return crudRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        return crudRepository.getByEmail(email);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return crudRepository.findAll(SORT_NAME_EMAIL);
+    }
+}
