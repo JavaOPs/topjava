2bf4a0f655b4144003d0de65c84023a2800a035b
IHOR PETROV <Basicprof@gmail.com>
2019-03-20 12:48:14 +0200

    6 03 extract rules

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -1,13 +1,11 @@
 package ru.javawebinar.topjava.service;
 
-import org.junit.AfterClass;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
+import org.junit.rules.ExternalResource;
 import org.junit.rules.Stopwatch;
-import org.junit.runner.Description;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
@@ -15,8 +13,7 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.javawebinar.topjava.ActiveDbProfileResolver;
-
-import java.util.concurrent.TimeUnit;
+import ru.javawebinar.topjava.TimingRules;
 
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
@@ -26,36 +23,17 @@ import java.util.concurrent.TimeUnit;
 @ActiveProfiles(resolver = ActiveDbProfileResolver.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 abstract public class AbstractServiceTest {
-    private static final Logger log = LoggerFactory.getLogger("result");
+    @ClassRule
+    public static ExternalResource summary = TimingRules.SUMMARY;
 
-    private static StringBuilder results = new StringBuilder();
+    @Rule
+    public Stopwatch stopwatch = TimingRules.STOPWATCH;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
-    @Rule
-    // http://stackoverflow.com/questions/14892125/what-is-the-best-practice-to-determine-the-execution-time-of-the-bussiness-relev
-    public Stopwatch stopwatch = new Stopwatch() {
-        @Override
-        protected void finished(long nanos, Description description) {
-            String result = String.format("%-95s %7d", description.getDisplayName(), TimeUnit.NANOSECONDS.toMillis(nanos));
-            results.append(result).append('\n');
-            log.info(result + " ms\n");
-        }
-    };
-
     static {
         // needed only for java.util.logging (postgres driver)
         SLF4JBridgeHandler.install();
-    }
-
-    @AfterClass
-    public static void printResult() {
-        log.info("\n-------------------------------------------------------------------------------------------------------" +
-                "\nTest                                                                                       Duration, ms" +
-                "\n-------------------------------------------------------------------------------------------------------\n" +
-                results +
-                "-------------------------------------------------------------------------------------------------------\n");
-        results.setLength(0);
     }
 }
