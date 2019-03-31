2f20f81fd81fa6169cce43f3cff18c4102c66c6b
IHOR PETROV <Basicprof@gmail.com>
2019-03-31 17:44:30 +0300

    7 07 controller test

---
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -0,0 +1,29 @@
+package ru.javawebinar.topjava.web;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.UserTestData.USER;
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
+
+public class RootControllerTest extends AbstractControllerTest {
+
+    @Test
+    public void testUsers() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("users"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
+                .andExpect(model().attribute("users", hasSize(2)))
+                .andExpect(model().attribute("users", hasItem(
+                        allOf(
+                                hasProperty("id", is(START_SEQ)),
+                                hasProperty("name", is(USER.getName()))
+                        )
+                )));
+    }
+}
