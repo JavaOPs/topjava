ee33d1e7bcc3bbc6e2740c7871eb178ed0d0f18e
IHOR PETROV <Basicprof@gmail.com>
2019-04-01 11:01:37 +0300

    7 09 rest controller

--- src/main/java/ru/javawebinar/topjava/web/user/AdminRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminRestController.java
@@ -1,40 +1,59 @@
 package ru.javawebinar.topjava.web.user;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.javawebinar.topjava.model.User;
 
+import java.net.URI;
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping(value = AdminRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class AdminRestController extends AbstractUserController {
 
+    public static final String REST_URL = "/rest/admin/users";
+
     @Override
+    @GetMapping
     public List<User> getAll() {
         return super.getAll();
     }
 
     @Override
-    public User get(int id) {
+    @GetMapping("/{id}")
+    public User get(@PathVariable int id) {
         return super.get(id);
     }
 
-    @Override
-    public User create(User user) {
-        return super.create(user);
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> createWithLocation(@RequestBody User user) {
+        User created = super.create(user);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
     }
 
     @Override
-    public void delete(int id) {
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
         super.delete(id);
     }
 
     @Override
-    public void update(User user, int id) {
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void update(@RequestBody User user, @PathVariable int id) {
         super.update(user, id);
     }
 
     @Override
-    public User getByMail(String email) {
+    @GetMapping("/by")
+    public User getByMail(@RequestParam String email) {
         return super.getByMail(email);
     }
 }
