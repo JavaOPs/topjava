bb0f631657c25ac2416a7e6f91d5cc872b3bdff5
IHOR PETROV <Basicprof@gmail.com>
2019-04-20 09:32:59 +0300

    10 07 interceptor

---
+++ src/main/java/ru/javawebinar/topjava/web/interceptor/ModelInterceptor.java
@@ -0,0 +1,25 @@
+package ru.javawebinar.topjava.web.interceptor;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.web.SecurityUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * This interceptor adds userTo to the model of every requests
+ */
+public class ModelInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        if (modelAndView != null && !modelAndView.isEmpty()) {
+            AuthorizedUser authorizedUser = SecurityUtil.safeGet();
+            if (authorizedUser != null) {
+                modelAndView.getModelMap().addAttribute("userTo", authorizedUser.getUserTo());
+            }
+        }
+    }
+}
