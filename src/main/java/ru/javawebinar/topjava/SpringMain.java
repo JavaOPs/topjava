3cf612fa8bc492f85dd68d481af014d97c3fccee
17sqrt2 <17pavels@gmail.com>
2019-06-17 22:12:29 +0500

    2 4 add spring context

---
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -0,0 +1,20 @@
+package ru.javawebinar.topjava;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.web.user.AdminRestController;
+
+import java.util.Arrays;
+
+public class SpringMain {
+    public static void main(String[] args) {
+        // java 7 automatic resource management
+        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
+            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
+        }
+    }
+}
