69d2dfeb4d8dcc0d92c949c592e2527a56c2cfbe
Antony.Glim <Antony.Glim@gmail.com>
2019-09-24 23:57:41 +0300

    HW0 first commit

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -26,6 +26,7 @@ public class UserMealsUtil {
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO return filtered list with correctly exceeded field
+        //
         return null;
     }
 }
