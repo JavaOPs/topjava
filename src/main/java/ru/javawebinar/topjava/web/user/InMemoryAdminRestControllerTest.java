8885eafa734cfc702a649425f99ed80ffc137018
Stanislav <rebelbob1337@gmail.com>
2019-10-27 15:15:01 +0400

    create inmemory test ctx

--- src/main/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
+++ src/main/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
@@ -23,7 +23,7 @@ public class InMemoryAdminRestControllerTest {
 
     @BeforeClass
     public static void beforeClass() {
-        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/inmemory.xml");
         log.info("\n{}\n", Arrays.toString(appCtx.getBeanDefinitionNames()));
         controller = appCtx.getBean(AdminRestController.class);
     }
