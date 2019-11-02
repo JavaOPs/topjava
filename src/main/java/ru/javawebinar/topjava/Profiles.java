881c689db4a0f6bb31abc05d4e522d162d0427e3
Pavel <ps07t447@gmail.com>
2019-11-02 13:19:43 +0300

    Apply 5_7_profile_resolver.patch

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -11,5 +11,18 @@ public class Profiles {
             POSTGRES_DB = "postgres",
             HSQL_DB = "hsqldb";
 
-    public static final String ACTIVE_DB = POSTGRES_DB;
+    //  Get DB profile depending of DB driver in classpath
+    public static String getActiveDbProfile() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return POSTGRES_DB;
+        } catch (ClassNotFoundException ex) {
+            try {
+                Class.forName("org.hsqldb.jdbcDriver");
+                return Profiles.HSQL_DB;
+            } catch (ClassNotFoundException e) {
+                throw new IllegalStateException("Could not find DB driver");
+            }
+        }
+    }
 }
