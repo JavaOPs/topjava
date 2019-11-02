51b149bb33129a72f8a07d41417d5f4e728448a4
Pavel Samodurov <ps07t447@gmail.com>
2019-11-02 13:52:27 +0300

    Apply 5_8_spring_data_jpa.patch

---
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -0,0 +1,41 @@
+package ru.javawebinar.topjava.repository.datajpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.MealRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public class DataJpaMealRepository implements MealRepository {
+
+    @Autowired
+    private CrudMealRepository crudRepository;
+
+    @Override
+    public Meal save(Meal meal, int userId) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(int id, int userId) {
+        return false;
+    }
+
+    @Override
+    public Meal get(int id, int userId) {
+        return null;
+    }
+
+    @Override
+    public List<Meal> getAll(int userId) {
+        return null;
+    }
+
+    @Override
+    public List<Meal> getBetweenInclusive(LocalDate startDate, LocalDate endDate, int userId) {
+        return null;
+    }
+}
