f8175345d815ea607dc38d90ede646d8d99b205a
admin <viewsonic12380@gmail.com>
2020-06-03 18:34:08 +0500

    prepare to HW0 patch

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -29,6 +29,7 @@ public class UserMealsUtil {
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO return filtered list with excess. Implement by cycles
+        System.out.println("hhh");
         return null;
     }
 
