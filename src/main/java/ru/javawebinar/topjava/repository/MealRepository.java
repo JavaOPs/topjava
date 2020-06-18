7985ccba1ee76229451842c86f087a37e659398a
Dvizova Oksana <vieviersa@gmail.com>
2020-06-18 17:33:15 +0300

    3_01_HW2_repositories.patch

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -2,17 +2,18 @@ package ru.javawebinar.topjava.repository;
 
 import ru.javawebinar.topjava.model.Meal;
 
-import java.util.Collection;
+import java.util.List;
 
 public interface MealRepository {
-    // null if not found, when updated
-    Meal save(Meal meal);
+    // null if updated meal do not belong to userId
+    Meal save(Meal meal, int userId);
 
-    // false if not found
-    boolean delete(int id);
+    // false if meal do not belong to userId
+    boolean delete(int id, int userId);
 
-    // null if not found
-    Meal get(int id);
+    // null if meal do not belong to userId
+    Meal get(int id, int userId);
 
-    Collection<Meal> getAll();
+    // ORDERED dateTime desc
+    List<Meal> getAll(int userId);
 }
--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
