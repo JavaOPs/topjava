acfd4a53e16dbcb2f2ad9374e6e91b1e19955e06
Dvizova Oksana <vieviersa@gmail.com>
2020-06-20 20:08:18 +0300

    3_09_add_junit.patch

---
+++ src/test/java/ru/javawebinar/topjava/UserTestData.java
@@ -0,0 +1,12 @@
+package ru.javawebinar.topjava;
+
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+
+public class UserTestData {
+    public static final int USER_ID = 1;
+    public static final int ADMIN_ID = 2;
+
+    public static final User USER = new User(USER_ID, "User", "user@yandex.ru", "password", Role.USER);
+    public static final User ADMIN = new User(ADMIN_ID, "Admin", "admin@gmail.com", "admin", Role.ADMIN);
+}
--- src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryBaseRepository.java
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryBaseRepository.java
@@ -11,7 +11,7 @@ public class InMemoryBaseRepository<T extends AbstractBaseEntity> {
 
     private static final AtomicInteger counter = new AtomicInteger(0);
 
-    private final Map<Integer, T> map = new ConcurrentHashMap<>();
+    final Map<Integer, T> map = new ConcurrentHashMap<>();
 
     public T save(T entry) {
         if (entry.isNew()) {
