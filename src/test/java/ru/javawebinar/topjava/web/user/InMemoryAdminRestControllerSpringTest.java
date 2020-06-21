4d6ac20ee1e55aac23dcb344b246f72b687e0a9e
Dvizova Oksana <vieviersa@gmail.com>
2020-06-21 15:11:35 +0300

    3_10_add_spring_test.patch

---
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -0,0 +1,40 @@
+package ru.javawebinar.topjava.web.user;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+
+@ContextConfiguration("classpath:spring/spring-app.xml")
+@RunWith(SpringRunner.class)
+public class InMemoryAdminRestControllerSpringTest {
+
+    @Autowired
+    private AdminRestController controller;
+
+    @Autowired
+    private InMemoryUserRepository repository;
+
+    @Before
+    public void setUp() throws Exception {
+        repository.init();
+    }
+
+    @Test
+    public void delete() throws Exception {
+        controller.delete(USER_ID);
+        Assert.assertNull(repository.get(USER_ID));
+    }
+
+    @Test
+    public void deleteNotFound() throws Exception {
+        Assert.assertThrows(NotFoundException.class, () -> controller.delete(10));
+    }
+}
