26c17f806cec5780d7f8fc4635dfddd61d0e3bec
Iron889 <timerbaev94@b.ru>
2020-09-22 17:37:03 +0300

    F

---
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -0,0 +1,46 @@
+package ru.javawebinar.topjava.model;
+
+import java.time.LocalDateTime;
+
+public class UserMealWithExcess {
+    private final LocalDateTime dateTime;
+
+    private final String description;
+
+    private final int calories;
+
+    private final boolean excess;
+
+    public UserMealWithExcess(LocalDateTime dateTime, String description, int calories, boolean excess) {
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+        this.excess = excess;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public boolean isExcess() {
+        return excess;
+    }
+
+    @Override
+    public String toString() {
+        return "UserMealWithExcess{" +
+                "dateTime=" + dateTime +
+                ", description='" + description + '\'' +
+                ", calories=" + calories +
+                ", excess=" + excess +
+                '}';
+    }
+}
