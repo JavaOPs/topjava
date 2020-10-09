c49b998a495ddc3e87eb904f019df6e3771a2c3e
Aleks-i <i.vanovaleksval@gmail.com>
2020-10-09 14:19:57 +0500

    Apply 2_3_app_layers.patch

---
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.topjava.service;
+
+import ru.javawebinar.topjava.repository.MealRepository;
+
+public class MealService {
+
+    private MealRepository repository;
+
+}
---
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -0,0 +1,38 @@
+package ru.javawebinar.topjava.service;
+
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFound;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
+
+public class UserService {
+
+    private UserRepository repository;
+
+    public User create(User user) {
+        return repository.save(user);
+    }
+
+    public void delete(int id) {
+        checkNotFoundWithId(repository.delete(id), id);
+    }
+
+    public User get(int id) {
+        return checkNotFoundWithId(repository.get(id), id);
+    }
+
+    public User getByEmail(String email) {
+        return checkNotFound(repository.getByEmail(email), "email=" + email);
+    }
+
+    public List<User> getAll() {
+        return repository.getAll();
+    }
+
+    public void update(User user) {
+        checkNotFoundWithId(repository.save(user), user.getId());
+    }
+}
