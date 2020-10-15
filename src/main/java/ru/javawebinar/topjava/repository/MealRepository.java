5d0ccee0cc6ed1380e1739e619e921b5d72191f7
Aleks-i <i.vanovaleksval@gmail.com>
2020-10-16 00:00:01 +0500

    Apply 3_0_refactoring.patch

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -5,14 +5,15 @@ import ru.javawebinar.topjava.model.Meal;
 import java.util.Collection;
 
 public interface MealRepository {
-    // null if not found, when updated
+    // null if updated meal do not belong to userId
     Meal save(Meal meal);
 
-    // false if not found
+    // false if meal do not belong to userId
     boolean delete(int id);
 
-    // null if not found
+    // null if meal do not belong to userId
     Meal get(int id);
 
+    // ORDERED dateTime desc
     Collection<Meal> getAll();
 }
