e384b83375cedcceb12f78c43b7c7bc858b3284e
Ilya <kineev.ilya@gmail.com>
2020-10-17 11:41:23 +0300

    prepare to 3_07_HW2_optional_select_user

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -4,8 +4,14 @@ import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
 
+    private static int id = 1;
+
     public static int authUserId() {
-        return 1;
+        return id;
+    }
+
+    public static void setAuthUserId(int id) {
+        SecurityUtil.id = id;
     }
 
     public static int authUserCaloriesPerDay() {
