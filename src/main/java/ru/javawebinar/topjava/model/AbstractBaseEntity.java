f23adfb37901e924b53643ec7bea68800b7dc4b7
Ilya <kineev.ilya@gmail.com>
2020-10-22 10:20:52 +0300

    prepare to 4_5_improve_code

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.model;
 
+import org.springframework.util.Assert;
+
 public abstract class AbstractBaseEntity {
     public static final int START_SEQ = 100000;
 
@@ -17,6 +19,12 @@ public abstract class AbstractBaseEntity {
     }
 
     public Integer getId() {
+        return id;
+    }
+
+    // doesn't work for hibernate lazy proxy
+    public int id() {
+        Assert.notNull(id, "Entity must has id");
         return id;
     }
 
