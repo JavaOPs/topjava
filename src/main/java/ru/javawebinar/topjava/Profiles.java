dcf2020044c199dd59d130a87cb5f6f018ee8977
Aleks-i <i.vanovaleksval@gmail.com>
2020-11-02 22:08:59 +0500

    Apply 5_7_spring_data_jpa.patch

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -3,9 +3,10 @@ package ru.javawebinar.topjava;
 public class Profiles {
     public static final String
             JDBC = "jdbc",
-            JPA = "jpa";
+            JPA = "jpa",
+            DATAJPA = "datajpa";
 
-    public static final String REPOSITORY_IMPLEMENTATION = JPA;
+    public static final String REPOSITORY_IMPLEMENTATION = DATAJPA;
 
     public static final String
             POSTGRES_DB = "postgres",
