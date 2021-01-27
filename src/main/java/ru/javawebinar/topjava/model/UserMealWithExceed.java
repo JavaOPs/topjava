4c8476ed1cb37f51d8c17193b28cbe5159a59dab
Sergey Polyakov <Griphone@mail.ru>
2021-01-28 01:19:53 +0300

    Prepare to HW0

---
+++ src/main/java/ru/javawebinar/topjava/model/UserMealWithExceed.java
@@ -0,0 +1,33 @@
+package ru.javawebinar.topjava.model;
+
+import java.time.LocalDateTime;
+
+public class UserMealWithExceed {
+    protected final LocalDateTime dateTime;
+    protected final String description;
+    protected final int calories;
+    protected final boolean exceed;
+
+    public UserMealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed) {
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+        this.exceed = exceed;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public boolean isExceed() {
+        return exceed;
+    }
+}
