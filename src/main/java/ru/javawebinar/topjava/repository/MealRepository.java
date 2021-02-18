d16b1e2930f75a67729cb9826492fa69228df607
Nikolay Rybakov <ni_fanta@mail.ru>
2021-02-18 14:41:49 +0300

    Apply 3_0_fix patch

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -5,13 +5,13 @@ import ru.javawebinar.topjava.model.Meal;
 import java.util.Collection;
 
 public interface MealRepository {
-    // null if updated meal do not belong to userId
+    // null if updated meal does not belong to userId
     Meal save(Meal meal);
 
-    // false if meal do not belong to userId
+    // false if meal does not belong to userId
     boolean delete(int id);
 
-    // null if meal do not belong to userId
+    // null if meal does not belong to userId
     Meal get(int id);
 
     // ORDERED dateTime desc
