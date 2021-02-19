8f139f2feeeecd0fdbc255fa5dd0bb8eda70652a
Nikolay Rybakov <ni_fanta@mail.ru>
2021-02-19 15:48:04 +0300

    Apply 3_13_test_UserService patch

--- src/main/java/ru/javawebinar/topjava/model/AbstractNamedEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractNamedEntity.java
@@ -4,6 +4,9 @@ public abstract class AbstractNamedEntity extends AbstractBaseEntity {
 
     protected String name;
 
+    public AbstractNamedEntity() {
+    }
+
     protected AbstractNamedEntity(Integer id, String name) {
         super(id);
         this.name = name;
---
+++ src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
@@ -0,0 +1,88 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.util.List;
+
+import static org.junit.Assert.assertThrows;
+import static ru.javawebinar.topjava.UserTestData.*;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class UserServiceTest {
+
+    @Autowired
+    private UserService service;
+
+    @Test
+    public void create() {
+        User newUser = getNew();
+        User created = service.create(newUser);
+        Integer newId = created.getId();
+        newUser.setId(newId);
+        assertMatch(created, newUser);
+        assertMatch(service.get(newId), newUser);
+    }
+
+    @Test
+    public void duplicateMailCreate() {
+        assertThrows(DataAccessException.class, () ->
+                service.create(new User(null, "Duplicate", "user@yandex.ru", "newPass", Role.USER)));
+    }
+
+    @Test
+    public void delete() {
+        service.delete(USER_ID);
+        assertThrows(NotFoundException.class, () -> service.get(USER_ID));
+    }
+
+    @Test
+    public void deletedNotFound() {
+        assertThrows(NotFoundException.class, () -> service.delete(NOT_FOUND));
+    }
+
+    @Test
+    public void get() {
+        User user = service.get(USER_ID);
+        assertMatch(user, UserTestData.user);
+    }
+
+    @Test
+    public void getNotFound() {
+        assertThrows(NotFoundException.class, () -> service.get(NOT_FOUND));
+    }
+
+    @Test
+    public void getByEmail() {
+        User user = service.getByEmail("admin@gmail.com");
+        assertMatch(user, admin);
+    }
+
+    @Test
+    public void update() {
+        User updated = getUpdated();
+        service.update(updated);
+        assertMatch(service.get(USER_ID), updated);
+    }
+
+    @Test
+    public void getAll() {
+        List<User> all = service.getAll();
+        assertMatch(all, admin, user);
+    }
+}
