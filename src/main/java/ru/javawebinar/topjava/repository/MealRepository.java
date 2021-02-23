a73fd08fa013a7420bda025af0186f8c75e687f0
AlekseyTonishev <alekseytonishev6@gmail.com>
2021-02-23 20:16:49 +0300

    3 01 HW2 repositories

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -5,14 +5,14 @@ import ru.javawebinar.topjava.model.Meal;
 import java.util.Collection;
 
 public interface MealRepository {
-    // null if updated meal do not belong to userId
-    Meal save(Meal meal);
+    // null if updated meal does not belong to userId
+    Meal save(Meal meal, int userId);
 
-    // false if meal do not belong to userId
-    boolean delete(int id);
+    // false if meal does not belong to userId
+    boolean delete(int id, int userId);
 
-    // null if meal do not belong to userId
-    Meal get(int id);
+    // null if meal does not belong to userId
+    Meal get(int id, int userId);
 
     // ORDERED dateTime desc
     Collection<Meal> getAll();
