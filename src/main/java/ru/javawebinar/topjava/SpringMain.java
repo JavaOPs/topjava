3ca31153258d2b32b4a5942e34be626f5f7dfde9
diesudmedexpert1995 <diesudmedexpert1995@gmail.com>
2021-03-08 12:18:06 +0200

    HW2RefactorRepo

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -24,7 +24,7 @@ public class SpringMain {
             System.out.println();
 
             MealRestController mealController = appCtx.getBean(MealRestController.class);
-           /* List<MealTo> filteredMealsWithExcess =
+           List<MealTo> filteredMealsWithExcess =
                     mealController.getBetween(
                             LocalDate.of(2020, Month.JANUARY, 30), LocalTime.of(7, 0),
                             LocalDate.of(2020, Month.JANUARY, 31), LocalTime.of(11, 0));
