25f6ae337fe4c9c10e410ffc556b776a71f52679
Dmitriy Kurochkin <kurochkin.dm@gmail.com>
2021-03-09 09:47:59 +0300

    3_13-3_016.patches

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -1,7 +1,12 @@
 package ru.javawebinar.topjava.model;
 
 public abstract class AbstractBaseEntity {
+    public static final int START_SEQ = 100000;
+
     protected Integer id;
+
+    public AbstractBaseEntity() {
+    }
 
     protected AbstractBaseEntity(Integer id) {
         this.id = id;
@@ -22,5 +27,22 @@ public abstract class AbstractBaseEntity {
     @Override
     public String toString() {
         return getClass().getSimpleName() + ":" + id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AbstractBaseEntity that = (AbstractBaseEntity) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id;
     }
 }
--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -1,10 +1,12 @@
 package ru.javawebinar.topjava.web;
 
+import ru.javawebinar.topjava.model.AbstractBaseEntity;
+
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
 
-    private static int id = 1;
+    private static int id = AbstractBaseEntity.START_SEQ;
 
     public static int authUserId() {
         return id;
