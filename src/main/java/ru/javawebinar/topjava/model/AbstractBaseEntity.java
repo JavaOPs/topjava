af565a57876d7df82295bbd2bd6c6496e7ce7e78
diesudmedexpert1995 <diesudmedexpert1995@gmail.com>
2021-03-09 18:09:47 +0200

    AddingUserTestSample

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.topjava.model;
 
 public abstract class AbstractBaseEntity {
+    public static final int START_SEQ = 100000;
+
     protected Integer id;
 
     protected AbstractBaseEntity(Integer id) {
@@ -22,5 +24,22 @@ public abstract class AbstractBaseEntity {
     @Override
     public String toString() {
         return getClass().getSimpleName() + ":" + id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AbstractBaseEntity that = (AbstractBaseEntity) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id;
     }
 }
