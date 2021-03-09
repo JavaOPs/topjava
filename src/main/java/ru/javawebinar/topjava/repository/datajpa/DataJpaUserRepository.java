3b5622f0b89b2db6fb232a059027d8a33e37fed0
AlekseyTonishev <alekseytonishev6@gmail.com>
2021-03-09 17:38:56 +0300

    5 7 spring data jpa

---
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserRepository.java
@@ -0,0 +1,44 @@
+package ru.javawebinar.topjava.repository.datajpa;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.List;
+
+@Repository
+public class DataJpaUserRepository implements UserRepository {
+    private static final Sort SORT_NAME_EMAIL = Sort.by(Sort.Direction.ASC, "name", "email");
+
+    private final CrudUserRepository crudRepository;
+
+    public DataJpaUserRepository(CrudUserRepository crudRepository) {
+        this.crudRepository = crudRepository;
+    }
+
+    @Override
+    public User save(User user) {
+        return crudRepository.save(user);
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return crudRepository.delete(id) != 0;
+    }
+
+    @Override
+    public User get(int id) {
+        return crudRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        return crudRepository.getByEmail(email);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return crudRepository.findAll(SORT_NAME_EMAIL);
+    }
+}
