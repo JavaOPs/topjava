0c8f59e084a76e8d423b9543521825954efd0bc4
diesudmedexpert1995 <diesudmedexpert1995@gmail.com>
2021-03-10 15:07:17 +0200

    HWImproveCode

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.model;
 
+import org.springframework.util.Assert;
+
 public abstract class AbstractBaseEntity {
     public static final int START_SEQ = 100000;
 
@@ -17,6 +19,11 @@ public abstract class AbstractBaseEntity {
     }
 
     public Integer getId() {
+        return id;
+    }
+
+    public int id() {
+        Assert.notNull(id, "Entity must have id");
         return id;
     }
 
--- src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryBaseRepository.java
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryBaseRepository.java
@@ -4,6 +4,7 @@ import ru.javawebinar.topjava.model.AbstractBaseEntity;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -16,6 +17,7 @@ public class InMemoryBaseRepository<T extends AbstractBaseEntity> {
     final Map<Integer, T> map = new ConcurrentHashMap<>();
 
     public T save(T entity) {
+        Objects.requireNonNull(entity, "Entity must not be null");
         if (entity.isNew()) {
             entity.setId(counter.getAndIncrement());
             map.put(entity.getId(), entity);
@@ -37,6 +39,7 @@ public class InMemoryBaseRepository<T extends AbstractBaseEntity> {
     }
 
     void put(T entity) {
-        map.put(entity.getId(), entity);
+        Objects.requireNonNull(entity, "Entity must not be null");
+        map.put(entity.id(), entity);
     }
 }
