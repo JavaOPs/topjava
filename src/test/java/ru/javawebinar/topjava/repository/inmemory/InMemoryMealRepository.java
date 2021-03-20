a4158fe19014f01b82bdedf68d85e700a462200a
Dmitriy Kurochkin <kurochkin.dm@gmail.com>
2021-03-20 11:59:41 +0300

    4.5 patch

--- src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -12,10 +12,7 @@ import ru.javawebinar.topjava.util.Util;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -36,6 +33,7 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public Meal save(Meal meal, int userId) {
+        Objects.requireNonNull(meal, "meal must not be null");
         InMemoryBaseRepository<Meal> meals = usersMealsMap.computeIfAbsent(userId, uid -> new InMemoryBaseRepository<>());
         return meals.save(meal);
     }
