98b809d82a8bf497be18c013629ebe237f273233
AlekseyTonishev <alekseytonishev6@gmail.com>
2021-03-30 19:01:17 +0300

    7 09 controller test

---
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -0,0 +1,32 @@
+package ru.javawebinar.topjava.web;
+
+import org.assertj.core.matcher.AssertionMatcher;
+import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.model.User;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.UserTestData.*;
+
+class RootControllerTest extends AbstractControllerTest {
+
+    @Test
+    void getUsers() throws Exception {
+        perform(get("/users"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("users"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
+                .andExpect(model().attribute("users",
+                        new AssertionMatcher<List<User>>() {
+                            @Override
+                            public void assertion(List<User> actual) throws AssertionError {
+                                USER_MATCHER.assertMatch(actual, admin, user);
+                            }
+                        }
+                ));
+    }
+}
