9ff770cab36ad1e3de4eea5ef040aa80668092ab
Dmitriy Kurochkin <kurochkin.dm@gmail.com>
2021-05-13 08:50:09 +0300

    7.15 patch

--- src/test/java/ru/javawebinar/topjava/TestMatcher.java
+++ src/test/java/ru/javawebinar/topjava/TestMatcher.java
@@ -1,29 +1,48 @@
 package ru.javawebinar.topjava;
 
+import org.springframework.test.web.servlet.ResultMatcher;
+
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static ru.javawebinar.topjava.TestUtil.readListFromJsonMvcResult;
 
 public class TestMatcher<T> {
+    private final Class<T> clazz;
     private final String[] fieldsToIgnore;
 
-    private TestMatcher(String... fieldsToIgnore) {
+    private TestMatcher(Class<T> clazz, String... fieldsToIgnore) {
+        this.clazz = clazz;
         this.fieldsToIgnore = fieldsToIgnore;
     }
 
-    public static <T> TestMatcher<T> usingIgnoringFieldsComparator(String... fieldsToIgnore) {
-        return new TestMatcher<>(fieldsToIgnore);
+    public static <T> TestMatcher<T> usingIgnoringFieldsComparator(Class<T> clazz, String... fieldsToIgnore) {
+        return new TestMatcher<>(clazz, fieldsToIgnore);
     }
 
     public void assertMatch(T actual, T expected) {
         assertThat(actual).usingRecursiveComparison().ignoringFields(fieldsToIgnore).isEqualTo(expected);
     }
 
-    public void assertMatch(Iterable<T> actual, T... expected) {
+    @SafeVarargs
+    public final void assertMatch(Iterable<T> actual, T... expected) {
         assertMatch(actual, List.of(expected));
     }
 
     public void assertMatch(Iterable<T> actual, Iterable<T> expected) {
         assertThat(actual).usingElementComparatorIgnoringFields(fieldsToIgnore).isEqualTo(expected);
+    }
+
+    public ResultMatcher contentJson(T expected) {
+        return result -> assertMatch(TestUtil.readFromJsonMvcResult(result, clazz), expected);
+    }
+
+    @SafeVarargs
+    public final ResultMatcher contentJson(T... expected) {
+        return contentJson(List.of(expected));
+    }
+
+    public ResultMatcher contentJson(Iterable<T> expected) {
+        return result -> assertMatch(readListFromJsonMvcResult(result, clazz), expected);
     }
 }
