d7dc9b52f7cbffaa14f0af2fd3247524ff9eabfb
wihrr <vihrowa.victoria@yandex.by>
2021-10-27 21:08:17 +0300

    3_09_add_junit

--- src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryBaseRepository.java
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryBaseRepository.java
@@ -9,9 +9,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class InMemoryBaseRepository<T extends AbstractBaseEntity> {
 
-    private static final AtomicInteger counter = new AtomicInteger(0);
+    protected static final AtomicInteger counter = new AtomicInteger(0);
 
-    private final Map<Integer, T> map = new ConcurrentHashMap<>();
+    protected final Map<Integer, T> map = new ConcurrentHashMap<>();
 
     public T save(T entity) {
         if (entity.isNew()) {
