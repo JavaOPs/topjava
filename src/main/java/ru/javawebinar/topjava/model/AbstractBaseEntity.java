25075b815cbfa32c552228b23602afa0502a2f43
login <loginow.evgeniy@gmail.com>
2021-11-16 14:26:08 +0400

    2 3 app layers

---
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -0,0 +1,26 @@
+package ru.javawebinar.topjava.model;
+
+public abstract class AbstractBaseEntity {
+    protected Integer id;
+
+    protected AbstractBaseEntity(Integer id) {
+        this.id = id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public boolean isNew() {
+        return this.id == null;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ":" + id;
+    }
+}
