75613cbfcfe9d31768b1452690c0be7757c8b825
Ilya Kyazimov <mister.kyazimov@yandex.ru>
2021-11-22 19:11:29 +0300

    1 1 HW0 streams

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.topjava.model;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Meal {
     private final LocalDateTime dateTime;
@@ -25,5 +27,13 @@ public class Meal {
 
     public int getCalories() {
         return calories;
+    }
+
+    public LocalDate getDate() {
+        return dateTime.toLocalDate();
+    }
+
+    public LocalTime getTime() {
+        return dateTime.toLocalTime();
     }
 }
