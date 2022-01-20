3126b76dc4b3128d9b5f9f9d61e19b86ceb610f2
Ilya Kyazimov <mister.kyazimov@yandex.ru>
2022-01-20 16:52:48 +0300

    7 09 controller test

---
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -0,0 +1,32 @@
+package ru.javawebinar.topjava.web;
+
+import org.junit.Test;
+import ru.javawebinar.topjava.UserTestData;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
+
+public class RootControllerTest extends AbstractControllerTest {
+
+    @Test
+    public void getUsers() throws Exception {
+        perform(get("/users"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("users"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
+                .andExpect(model().attribute("users", hasSize(2)))
+                .andExpect(model().attribute("users", hasItem(
+                        allOf(
+                                hasProperty("id", is(START_SEQ)),
+                                hasProperty("name", is(UserTestData.user.getName()))
+                        )
+                )));
+    }
+}
