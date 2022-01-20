053925a4570f6549c0656d503550d1fe29f8dbcd
OGordievsky <og87@bk.ru>
2022-01-20 17:07:34 +0300

    Realesed method UserMealsUtil.filteredByCycles. This realisation good working only with sorted by per days List<UserMeal> meals method param.

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -6,8 +6,7 @@ import ru.javawebinar.topjava.model.UserMealWithExcess;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -21,15 +20,46 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
         );
 
-        List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000); //default 7:00 to 12:00 cl 2000
         mealsTo.forEach(System.out::println);
 
 //        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+
+        // It works if this method get only a sorted by per day list meals
+
+        List<UserMealWithExcess> resultMealList = new ArrayList<>();
+        List<UserMeal> dayUserMeal = new ArrayList<>();
+        int currentDay = 0;
+        int dayCal = 0;
+
+        for (UserMeal userMeal : meals) {
+            LocalDateTime lcDateTime = userMeal.getDateTime();
+            int day = lcDateTime.getDayOfMonth();
+            if (currentDay != day) {
+                currentDay = day;
+                if (!dayUserMeal.isEmpty()) {
+                    for (UserMeal dayMeal : dayUserMeal){
+                        resultMealList.add(new UserMealWithExcess(dayMeal.getDateTime(), dayMeal.getDescription(), dayMeal.getCalories(), dayCal>caloriesPerDay));
+                    }
+                    dayUserMeal.clear();
+                }
+                dayCal = 0;
+            }
+            dayCal += userMeal.getCalories();
+            if (TimeUtil.isBetweenHalfOpen(lcDateTime.toLocalTime(), startTime, endTime)) {
+                dayUserMeal.add(userMeal);
+            }
+        }
+        if (!dayUserMeal.isEmpty()) {
+            for (UserMeal dayMeal : dayUserMeal){
+                resultMealList.add(new UserMealWithExcess(dayMeal.getDateTime(), dayMeal.getDescription(), dayMeal.getCalories(), dayCal>caloriesPerDay));
+            }
+        }
         // TODO return filtered list with excess. Implement by cycles
-        return null;
+        return resultMealList;
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
