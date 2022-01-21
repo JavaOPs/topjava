d13f0e49855dbdf75a514c6c2eb2b7d0e2a090a9
OGordievsky <og87@bk.ru>
2022-01-21 15:32:20 +0300

    Refactor method UserMealsUtil.filteredByCycles. Added default sorting list meals in-to method body.

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -28,21 +28,30 @@ public class UserMealsUtil {
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
 
-        // It works if this method get only a sorted by per day list meals
+        // Sorted by data -> for good working. If the param "meals" always gets only sorted list by per day, next 2 line code can be hide or remote,
+        // with refactor name variable in "for:each" cycle [1*]
+
+        List<UserMeal> inputMeals = meals;                                  //1 line
+        inputMeals.sort(Comparator.comparing(UserMeal::getDateTime));       //2 line
+
+        // Temp variables:
 
         List<UserMealWithExcess> resultMealList = new ArrayList<>();
         List<UserMeal> dayUserMeal = new ArrayList<>();
         int currentDay = 0;
         int dayCal = 0;
 
-        for (UserMeal userMeal : meals) {
+        // Next code works if list meals sorted by per day
+
+        for (UserMeal userMeal : inputMeals) { //[1*] refactor inputMeals to meals
             LocalDateTime lcDateTime = userMeal.getDateTime();
             int day = lcDateTime.getDayOfMonth();
             if (currentDay != day) {
                 currentDay = day;
                 if (!dayUserMeal.isEmpty()) {
-                    for (UserMeal dayMeal : dayUserMeal){
-                        resultMealList.add(new UserMealWithExcess(dayMeal.getDateTime(), dayMeal.getDescription(), dayMeal.getCalories(), dayCal>caloriesPerDay));
+                    //filling from temp list dayUserMeal to resultMealList if next userMeal has next day
+                    for (UserMeal dayMeal : dayUserMeal) {
+                        resultMealList.add(new UserMealWithExcess(dayMeal.getDateTime(), dayMeal.getDescription(), dayMeal.getCalories(), dayCal > caloriesPerDay));
                     }
                     dayUserMeal.clear();
                 }
@@ -53,9 +62,10 @@ public class UserMealsUtil {
                 dayUserMeal.add(userMeal);
             }
         }
+        // Finish fill resultMealList from temp list dayUserMeal
         if (!dayUserMeal.isEmpty()) {
-            for (UserMeal dayMeal : dayUserMeal){
-                resultMealList.add(new UserMealWithExcess(dayMeal.getDateTime(), dayMeal.getDescription(), dayMeal.getCalories(), dayCal>caloriesPerDay));
+            for (UserMeal dayMeal : dayUserMeal) {
+                resultMealList.add(new UserMealWithExcess(dayMeal.getDateTime(), dayMeal.getDescription(), dayMeal.getCalories(), dayCal > caloriesPerDay));
             }
         }
         // TODO return filtered list with excess. Implement by cycles
