cc8825543950062c811a23355fb6c730122c1439
Ilya Kyazimov <mister.kyazimov@yandex.ru>
2022-01-27 19:52:19 +0300

    HW8

--- src/main/webapp/resources/js/topjava.users.js
+++ src/main/webapp/resources/js/topjava.users.js
@@ -2,8 +2,26 @@ const userAjaxUrl = "admin/users/";
 
 // https://stackoverflow.com/a/5064235/548473
 const ctx = {
-    ajaxUrl: userAjaxUrl
+    ajaxUrl: userAjaxUrl,
+    updateTable: function () {
+        $.get(userAjaxUrl, updateTableByData);
+    }
 };
+
+function enable(chkbox, id) {
+    let enabled = chkbox.is(":checked");
+    //  https://stackoverflow.com/a/22213543/548473
+    $.ajax({
+            url: userAjaxUrl + id,
+            type: "POST",
+        data: "enabled=" + enabled
+    }).done(function () {
+        chkbox.closest("tr").attr("data-user-enabled", enabled);
+        successNoty(enabled ? "Enabled" : "Disabled");
+    }).fail(function () {
+        $(chkbox).prop("checked", !enabled);
+    });
+}
 
 // $(document).ready(function () {
 $(function () {
