c2e6c648e44af9eec62a781dfe05ff0871fddf99
Aleksey Ilin <kayrus@mail.ru>
2022-02-03 22:38:09 +0300

    1-3 HW0 opt2

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -9,12 +9,25 @@ public class MealTo {
 
     private final int calories;
 
-    private final boolean excess;
+//    private final AtomicBoolean excess;      // filteredByAtomic (or any ref type, e.g. boolean[1])
+//    private final Boolean excess;            // filteredByReflection
+//    private final Supplier<Boolean> excess;  // filteredByClosure
+    private boolean excess;
 
     public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
+        this.excess = excess;
+    }
+
+//    for filteredByClosure
+//    public Boolean getExcess() {
+//        return excess.get();
+//    }
+
+    // for filteredBySetterRecursion
+    public void setExcess(boolean excess) {
         this.excess = excess;
     }
 
