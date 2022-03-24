500eb6b5cc5c63c463e722a472d9149ca2d8c0a8
xdevil013 <xdevil013@gmail.com>
2022-03-24 12:53:47 +0300

    add mvc test

--- src/test/java/ru/javawebinar/topjava/ActiveDbProfileResolver.java
+++ src/test/java/ru/javawebinar/topjava/ActiveDbProfileResolver.java
@@ -1,13 +1,19 @@
 package ru.javawebinar.topjava;
 
 import org.springframework.lang.NonNull;
-import org.springframework.test.context.ActiveProfilesResolver;
+import org.springframework.test.context.support.DefaultActiveProfilesResolver;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 //http://stackoverflow.com/questions/23871255/spring-profiles-simple-example-of-activeprofilesresolver
-public class ActiveDbProfileResolver implements ActiveProfilesResolver {
+public class ActiveDbProfileResolver extends DefaultActiveProfilesResolver {
     @Override
     public @NonNull
     String[] resolve(@NonNull Class<?> aClass) {
-        return new String[]{Profiles.getActiveDbProfile()};
+        List<String> profiles = new ArrayList<>(Arrays.asList(super.resolve(aClass)));
+        profiles.add(Profiles.getActiveDbProfile());
+        return profiles.toArray(String[]::new);
     }
 }
