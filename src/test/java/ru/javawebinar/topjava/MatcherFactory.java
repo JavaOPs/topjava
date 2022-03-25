82c0d960af3d39b7f480b170227076380c67834a
xdevil013 <xdevil013@gmail.com>
2022-03-25 11:00:55 +0300

    add admin and profile tests

--- src/test/java/ru/javawebinar/topjava/MatcherFactory.java
+++ src/test/java/ru/javawebinar/topjava/MatcherFactory.java
@@ -1,5 +1,11 @@
 package ru.javawebinar.topjava;
 
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+import ru.javawebinar.topjava.web.json.JsonUtil;
+
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,16 +14,19 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Factory for creating test matchers.
  * <p>
  * Comparing actual and expected objects via AssertJ
+ * Support converting json MvcResult to objects for comparation.
  */
 public class MatcherFactory {
-    public static <T> Matcher<T> usingIgnoringFieldsComparator(String... fieldsToIgnore) {
-        return new Matcher<>(fieldsToIgnore);
+    public static <T> Matcher<T> usingIgnoringFieldsComparator(Class<T> clazz, String... fieldsToIgnore) {
+        return new Matcher<>(clazz, fieldsToIgnore);
     }
 
     public static class Matcher<T> {
+        private final Class<T> clazz;
         private final String[] fieldsToIgnore;
 
-        private Matcher(String... fieldsToIgnore) {
+        private Matcher(Class<T> clazz, String... fieldsToIgnore) {
+            this.clazz = clazz;
             this.fieldsToIgnore = fieldsToIgnore;
         }
 
@@ -32,6 +41,27 @@ public class MatcherFactory {
 
         public void assertMatch(Iterable<T> actual, Iterable<T> expected) {
             assertThat(actual).usingRecursiveFieldByFieldElementComparatorIgnoringFields(fieldsToIgnore).isEqualTo(expected);
+        }
+
+        public ResultMatcher contentJson(T expected) {
+            return result -> assertMatch(JsonUtil.readValue(getContent(result), clazz), expected);
+        }
+
+        @SafeVarargs
+        public final ResultMatcher contentJson(T... expected) {
+            return contentJson(List.of(expected));
+        }
+
+        public ResultMatcher contentJson(Iterable<T> expected) {
+            return result -> assertMatch(JsonUtil.readValues(getContent(result), clazz), expected);
+        }
+
+        public T readFromJson(ResultActions action) throws UnsupportedEncodingException {
+            return JsonUtil.readValue(getContent(action.andReturn()), clazz);
+        }
+
+        private static String getContent(MvcResult result) throws UnsupportedEncodingException {
+            return result.getResponse().getContentAsString();
         }
     }
 }
