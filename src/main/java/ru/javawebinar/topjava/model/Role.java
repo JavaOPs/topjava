2e681b3089ea518b1e993f6cd533efa882c77ab1
xdevil013 <xdevil013@gmail.com>
2022-04-12 10:21:36 +0300

    add security authenticated provider

--- src/main/java/ru/javawebinar/topjava/model/Role.java
+++ src/main/java/ru/javawebinar/topjava/model/Role.java
@@ -1,6 +1,14 @@
 package ru.javawebinar.topjava.model;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
     USER,
-    ADMIN
+    ADMIN;
+
+//    https://stackoverflow.com/a/19542316/548473
+    @Override
+    public String getAuthority() {
+        return "ROLE_" + name();
+    }
 }
--- src/main/java/ru/javawebinar/topjava/to/UserTo.java
+++ src/main/java/ru/javawebinar/topjava/to/UserTo.java
@@ -1,10 +1,18 @@
 package ru.javawebinar.topjava.to;
 
+import org.hibernate.validator.constraints.Range;
+import ru.javawebinar.topjava.util.UserUtil;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.io.Serial;
+import java.io.Serializable;
 
-public class UserTo extends BaseTo {
+public class UserTo extends BaseTo implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @NotBlank
     @Size(min = 2, max = 100)
@@ -19,14 +27,19 @@ public class UserTo extends BaseTo {
     @Size(min = 5, max = 32, message = "length must be between 5 and 32 characters")
     private String password;
 
+    @Range(min = 10, max = 10000)
+    @NotNull
+    private Integer caloriesPerDay = UserUtil.DEFAULT_CALORIES_PER_DAY;
+
     public UserTo() {
     }
 
-    public UserTo(Integer id, String name, String email, String password) {
+    public UserTo(Integer id, String name, String email, String password, int caloriesPerDay) {
         super(id);
         this.name = name;
         this.email = email;
         this.password = password;
+        this.caloriesPerDay = caloriesPerDay;
     }
 
     public String getPassword() {
@@ -53,12 +66,17 @@ public class UserTo extends BaseTo {
         this.email = email;
     }
 
+    public Integer getCaloriesPerDay() {
+        return caloriesPerDay;
+    }
+
     @Override
     public String toString() {
         return "UserTo{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
+                ", caloriesPerDay='" + caloriesPerDay + '\'' +
                 '}';
     }
 }
