45ab7e08cc4fcc3a488c7aaaeabd80346c5437e0
Alexei Valchuk <a.valchukav@gmail.com>
2023-02-13 17:53:18 +0300

    refactor: add InMemoryBaseRepository to avoid code duplication

---
+++ src/main/java/ru/javawebinar/topjava/repository/inMemory/InMemoryBaseRepository.java
@@ -0,0 +1,47 @@
+package ru.javawebinar.topjava.repository.inMemory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.AbstractBaseEntity;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author Alexei Valchuk, 13.02.2023, email: a.valchukav@gmail.com
+ */
+
+@Repository
+public class InMemoryBaseRepository<T extends AbstractBaseEntity> {
+
+    protected static final Logger LOG = LoggerFactory.getLogger(InMemoryUserRepository.class);
+
+    private static final AtomicInteger counter = new AtomicInteger(0);
+
+    private final Map<Integer, T> repository = new ConcurrentHashMap<>();
+
+    public T save(T entry) {
+        LOG.info("save {}", entry);
+        if (entry.isNew()) {
+            entry.setId(counter.incrementAndGet());
+            repository.put(entry.getId(), entry);
+            return entry;
+        }
+        return repository.computeIfPresent(entry.getId(), (id, oldT) -> entry);
+    }
+
+    public boolean delete(int id) {
+        return repository.remove(id) != null;
+    }
+
+    public T get(int id) {
+        return repository.get(id);
+    }
+
+    Collection<T> getCollection() {
+        return repository.values();
+    }
+}
