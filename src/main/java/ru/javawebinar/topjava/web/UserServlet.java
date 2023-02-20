3db22fa3c01ac6e4621b2cd251785e6edb31e6c1
ruslan <medtehnik850@gmail.com>
2023-02-20 19:25:57 +0700

    Lesson10: Add logging

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,16 +1,22 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class UserServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(UserServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirect to userList");
         response.sendRedirect("/users.jsp");
-     //   request.getRequestDispatcher("/users.jsp").forward(request, response);
+        //   request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
 }
