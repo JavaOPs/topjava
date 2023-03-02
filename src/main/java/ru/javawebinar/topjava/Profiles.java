fbc3a607730894b1c61bde6e6ec3f5d1aa46edf1
Ilya547 <Ilya547@bk.ru>
2023-03-02 13:28:33 +0400

    5_6_profile_resolver.patch

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava;
 
+import org.springframework.util.ClassUtils;
+
 public class Profiles {
     public static final String
             JDBC = "jdbc",
@@ -11,5 +13,14 @@ public class Profiles {
             POSTGRES_DB = "postgres",
             HSQL_DB = "hsqldb";
 
-    public static final String ACTIVE_DB = POSTGRES_DB;
+    //  Get DB profile depending of DB driver in classpath
+    public static String getActiveDbProfile() {
+        if (ClassUtils.isPresent("org.postgresql.Driver", null)) {
+            return POSTGRES_DB;
+        } else if (ClassUtils.isPresent("org.hsqldb.jdbcDriver", null)) {
+            return HSQL_DB;
+        } else {
+            throw new IllegalStateException("Could not find DB driver");
+        }
+    }
 }
