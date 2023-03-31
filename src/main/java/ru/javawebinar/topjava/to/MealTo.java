85b2b0e8b34c6958e3b810bf7924eaf2b15d8bd0
Ilya547 <Ilya547@bk.ru>
2023-03-31 13:12:29 +0300

    8_01_HW07_controller_test.patch

--- src/main/java/ru/javawebinar/topjava/to/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/to/MealTo.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.to;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class MealTo {
     private final Integer id;
@@ -39,6 +40,23 @@ public class MealTo {
 
     public boolean isExcess() {
         return excess;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MealTo mealTo = (MealTo) o;
+        return calories == mealTo.calories &&
+                excess == mealTo.excess &&
+                Objects.equals(id, mealTo.id) &&
+                Objects.equals(dateTime, mealTo.dateTime) &&
+                Objects.equals(description, mealTo.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, dateTime, description, calories, excess);
     }
 
     @Override
