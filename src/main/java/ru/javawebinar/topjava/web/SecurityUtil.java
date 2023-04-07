4fedb2189e7f03143ccafe27404f346173af082f
Ilya547 <Ilya547@bk.ru>
2023-04-07 19:54:30 +0300

    9_11_auth_via_user_service.patch

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -1,25 +1,34 @@
 package ru.javawebinar.topjava.web;
 
-import ru.javawebinar.topjava.model.AbstractBaseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import ru.javawebinar.topjava.AuthorizedUser;
 
-import static ru.javawebinar.topjava.util.UsersUtil.DEFAULT_CALORIES_PER_DAY;
+import static java.util.Objects.requireNonNull;
 
 public class SecurityUtil {
-
-    private static int id = AbstractBaseEntity.START_SEQ;
 
     private SecurityUtil() {
     }
 
-    public static int authUserId() {
-        return id;
+    public static AuthorizedUser safeGet() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null) {
+            return null;
+        }
+        Object principal = auth.getPrincipal();
+        return (principal instanceof AuthorizedUser) ? (AuthorizedUser) principal : null;
     }
 
-    public static void setAuthUserId(int id) {
-        SecurityUtil.id = id;
+    public static AuthorizedUser get() {
+        return requireNonNull(safeGet(), "No authorized user found");
+    }
+
+    public static int authUserId() {
+        return get().getUserTo().id();
     }
 
     public static int authUserCaloriesPerDay() {
-        return DEFAULT_CALORIES_PER_DAY;
+        return get().getUserTo().getCaloriesPerDay();
     }
 }
