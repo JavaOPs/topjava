f71be2215998fb4a5a43cd908571f8cced91a207
Ilya547 <Ilya547@bk.ru>
2023-04-07 19:54:52 +0300

    9_12_spring_security_test.patch

--- src/test/java/ru/javawebinar/topjava/web/AbstractControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/AbstractControllerTest.java
@@ -17,6 +17,9 @@ import ru.javawebinar.topjava.Profiles;
 
 import javax.annotation.PostConstruct;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+
 @SpringJUnitWebConfig(locations = {
         "classpath:spring/spring-app.xml",
         "classpath:spring/spring-mvc.xml",
@@ -52,6 +55,7 @@ public abstract class AbstractControllerTest {
         mockMvc = MockMvcBuilders
                 .webAppContextSetup(webApplicationContext)
                 .addFilter(CHARACTER_ENCODING_FILTER)
+                .apply(springSecurity())
                 .build();
     }
 
