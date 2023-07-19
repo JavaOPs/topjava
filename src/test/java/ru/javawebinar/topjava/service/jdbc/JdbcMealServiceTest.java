a31f5f9cbd115cb3f4d180d5e36f8b8a25ec58f9
Grigorii <grigoriisoldatov>
2023-07-19 17:53:18 +0300

    6_09_hibernate_cache

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
@@ -1,10 +1,12 @@
 package ru.javawebinar.topjava.service.jdbc;
 
+import org.junit.Ignore;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
+@Ignore
 public class JdbcMealServiceTest extends AbstractMealServiceTest {
 }
