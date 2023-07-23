d5d12f5926dd3d68c8f859695da66c28d261ad69
Grigorii <grigoriisoldatov>
2023-07-23 17:33:48 +0300

    6_14_spring_webmvc

---
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -0,0 +1,41 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.javawebinar.topjava.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class RootController {
+    private static final Logger log = LoggerFactory.getLogger(RootController.class);
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/")
+    public String root() {
+        log.info("root");
+        return "index";
+    }
+
+    @GetMapping("/users")
+    public String getUsers(Model model) {
+        log.info("users");
+        model.addAttribute("users", service.getAll());
+        return "users";
+    }
+
+    @PostMapping("/users")
+    public String setUser(HttpServletRequest request) {
+        int userId = Integer.parseInt(request.getParameter("userId"));
+        log.info("setUser {}", userId);
+        SecurityUtil.setAuthUserId(userId);
+        return "redirect:meals";
+    }
+}
