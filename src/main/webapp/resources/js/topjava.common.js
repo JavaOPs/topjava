b1bc0704ead45489819900a6af78f2b0471337fb
Grigorii <grigoriisoldatov>
2023-08-11 17:19:34 +0300

    8_09_ajax_datatables

---
+++ src/main/webapp/resources/js/topjava.common.js
@@ -0,0 +1,39 @@
+function makeEditable() {
+    $(".delete").click(function () {
+        deleteRow($(this).attr("id"));
+    });
+}
+
+function add() {
+    $("#detailsForm").find(":input").val("");
+    $("#editRow").modal();
+}
+
+function deleteRow(id) {
+    $.ajax({
+        url: ajaxUrl + id,
+        type: "DELETE",
+        success: function () {
+            updateTable();
+        }
+    });
+}
+
+function updateTable() {
+    $.get(ajaxUrl, function (data) {
+        datatableApi.clear().rows.add(data).draw();
+    });
+}
+
+function save() {
+    const form = $("#detailsForm");
+    $.ajax({
+        type: "POST",
+        url: ajaxUrl,
+        data: form.serialize(),
+        success: function () {
+            $("#editRow").modal("hide");
+            updateTable();
+        }
+    });
+}
