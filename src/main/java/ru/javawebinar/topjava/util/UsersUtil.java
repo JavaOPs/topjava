dffbac06e9839a76b6e1473621edca983dc3dab0
Grigorii <grigoriisoldatov>
2023-08-13 11:58:58 +0300

    9_11_auth_via_user_service

--- src/main/java/ru/javawebinar/topjava/util/UsersUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UsersUtil.java
@@ -12,9 +12,14 @@ public class UsersUtil {
         return new User(null, userTo.getName(), userTo.getEmail().toLowerCase(), userTo.getPassword(), Role.USER);
     }
 
+    public static UserTo asTo(User user) {
+        return new UserTo(user.getId(), user.getName(), user.getEmail(), user.getPassword(), user.getCaloriesPerDay());
+    }
+
     public static User updateFromTo(User user, UserTo userTo) {
         user.setName(userTo.getName());
         user.setEmail(userTo.getEmail().toLowerCase());
+        user.setCaloriesPerDay(userTo.getCaloriesPerDay());
         user.setPassword(userTo.getPassword());
         return user;
     }
