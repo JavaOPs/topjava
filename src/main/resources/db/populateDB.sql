2505a8913b9640ae6523c000cd7103dfbc96906c
Grigorii <grigoriisoldatov>
2023-08-20 17:19:31 +0300

    10_08_profile_jsptag

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -3,10 +3,10 @@ DELETE FROM meal;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
-INSERT INTO users (name, email, password)
-VALUES ('User', 'user@yandex.ru', 'password'),
-       ('Admin', 'admin@gmail.com', 'admin'),
-       ('Guest', 'guest@gmail.com', 'guest');
+INSERT INTO users (name, email, password, calories_per_day)
+VALUES ('User', 'user@yandex.ru', 'password', 2005),
+       ('Admin', 'admin@gmail.com', 'admin', 1900),
+       ('Guest', 'guest@gmail.com', 'guest', 2000);
 
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
