0ae2d1891e8d8fd56ff904975a36e868c23cf265
avnik <a.v.nikolaef@gmail.com>
2023-08-29 21:23:33 +0300

    HW1 2.0 fix demo link

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -1,7 +1,7 @@
 package ru.javawebinar.topjava;
 
 /**
- * @see <a href="http://topjava.herokuapp.com">Demo application</a>
+ * @see <a href="http://javaops-demo.ru/topjava">Demo application</a>
  * @see <a href="https://github.com/JavaOPs/topjava">Initial project</a>
  */
 public class Main {
