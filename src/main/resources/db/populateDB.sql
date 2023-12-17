affffc981fbefbde67426d5f62ef5081fdad8399
dima <dimvalyaev@gmai.com>
2023-12-17 18:56:03 +0500

    db implementation

---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,12 @@
+DELETE FROM user_role;
+DELETE FROM users;
+ALTER SEQUENCE global_seq RESTART WITH 100000;
+
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin'),
+       ('Guest', 'guest@gmail.com', 'guest');
+
+INSERT INTO user_role (role, user_id)
+VALUES ('USER', 100000),
+       ('ADMIN', 100001);
