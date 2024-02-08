59dd1eb80c6b2ee3869482cf813c5dcfd2673b62
Anton <k_anton1@mail.ru>
2024-02-08 18:51:27 +0300

    Brought to accordance with master@https://github.com/JavaWebinar/topjava.git

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -9,25 +9,12 @@ public class MealTo {
 
     private final int calories;
 
-//    private final AtomicBoolean excess;      // filteredByAtomic (or any ref type, e.g. boolean[1])
-//    private final Boolean excess;            // filteredByReflection
-//    private final Supplier<Boolean> excess;  // filteredByClosure
-    private boolean excess;
+    private final boolean excess;
 
     public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
-        this.excess = excess;
-    }
-
-//    for filteredByClosure
-//    public Boolean getExcess() {
-//        return excess.get();
-//    }
-
-    // for filteredBySetterRecursion
-    public void setExcess(boolean excess) {
         this.excess = excess;
     }
 
