ed42b7183ac520062cf01fb86d4eddd240137ded
dima <dimvalyaev@gmai.com>
2024-02-19 10:35:39 +0700

    4_8_add_jpa

--- src/main/java/ru/javawebinar/topjava/model/AbstractNamedEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractNamedEntity.java
@@ -1,7 +1,17 @@
 package ru.javawebinar.topjava.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+@MappedSuperclass
 public abstract class AbstractNamedEntity extends AbstractBaseEntity {
 
+    @NotBlank
+    @Size(min = 2, max = 128)
+    @Column(name = "name", nullable = false)
     protected String name;
 
     protected AbstractNamedEntity() {
