6ae8b57a369629882444178f6132800ac4a8bf5d
dima <dimvalyaev@gmai.com>
2024-03-06 15:24:17 +0700

    5_7_spring_data_jpa

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,9 +1,6 @@
 package ru.javawebinar.topjava.service;
 
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.rules.Stopwatch;
 import org.junit.runner.Description;
 import org.junit.runner.RunWith;
@@ -36,6 +33,7 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 @ActiveProfiles(resolver = ActiveDbProfileResolver.class)
+@Ignore
 public class MealServiceTest {
     private static final Logger log = getLogger("result");
 
