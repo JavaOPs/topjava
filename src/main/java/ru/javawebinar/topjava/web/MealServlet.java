819b2067deca4e8f23104e83a7868a3b31af4c94
dima <dimvalyaev@gmai.com>
2024-03-08 09:06:44 +0700

    Revert "fix servlet"
    This reverts commit be831887ba6d36a1acfd290e9995fbc56b356a6d.

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -27,7 +27,7 @@ public class MealServlet extends HttpServlet {
 
     @Override
     public void init() {
-        springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml");
         mealController = springContext.getBean(MealRestController.class);
     }
 
