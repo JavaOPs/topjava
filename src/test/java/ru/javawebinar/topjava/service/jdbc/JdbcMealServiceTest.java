13febfd50b8fe4ff44041c7b2da8f067d07246ab
kaklyugin <kaklyugin@anc.com>
2024-03-27 11:34:39 +0300

    Fixed JSP,tests, beans. Reason: https://topjava31hw-w.slack.com/archives/C06G9TJ7QFQ/p1710846852147599

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.service.jdbc;
 
-import org.junit.Ignore;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
 
@@ -8,8 +7,4 @@ import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
 public class JdbcMealServiceTest extends AbstractMealServiceTest {
-    @Override
-    @Ignore
-    public void createWithException() throws Exception {
-    }
 }
