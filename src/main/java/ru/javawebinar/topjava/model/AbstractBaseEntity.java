d30e8a94fba30b3abafe032840ff91a12e877c25
kaklyugin <kaklyugin@anc.com>
2024-04-14 11:48:25 +0300

    9_05_update

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -1,7 +1,6 @@
 package ru.javawebinar.topjava.model;
 
-import org.springframework.data.domain.Persistable;
-import org.springframework.util.Assert;
+import ru.javawebinar.topjava.HasId;
 
 import javax.persistence.*;
 
@@ -11,7 +10,7 @@ import static ru.javawebinar.topjava.util.Util.getEffectiveClass;
 // http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
 @Access(AccessType.FIELD)
 //@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, isGetterVisibility = NONE, setterVisibility = NONE)
-public abstract class AbstractBaseEntity implements Persistable<Integer> {
+public abstract class AbstractBaseEntity implements HasId {
     public static final int START_SEQ = 100000;
 
     @Id
@@ -29,6 +28,7 @@ public abstract class AbstractBaseEntity implements Persistable<Integer> {
         this.id = id;
     }
 
+    @Override
     public void setId(Integer id) {
         this.id = id;
     }
@@ -36,16 +36,6 @@ public abstract class AbstractBaseEntity implements Persistable<Integer> {
     @Override
     public Integer getId() {
         return id;
-    }
-
-    public int id() {
-        Assert.notNull(id, "Entity must have id");
-        return id;
-    }
-
-    @Override
-    public boolean isNew() {
-        return this.id == null;
     }
 
     @Override
