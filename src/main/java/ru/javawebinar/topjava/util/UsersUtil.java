a62121519a7851310247f4a0dbf94e75be9886e2
kaklyugin <kaklyugin@anc.com>
2024-04-14 11:47:51 +0300

    9_04_binding

---
+++ src/main/java/ru/javawebinar/topjava/util/UsersUtil.java
@@ -0,0 +1,14 @@
+package ru.javawebinar.topjava.util;
+
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.to.UserTo;
+
+public class UsersUtil {
+
+    public static final int DEFAULT_CALORIES_PER_DAY = 2000;
+
+    public static User createNewFromTo(UserTo userTo) {
+        return new User(null, userTo.getName(), userTo.getEmail().toLowerCase(), userTo.getPassword(), Role.USER);
+    }
+}
