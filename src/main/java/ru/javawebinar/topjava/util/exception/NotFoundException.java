2ddf115673f195ab52c9ee84b7f9b97700e260e9
kaklyugin <kaklyugin@anc.com>
2024-04-14 12:17:13 +0300

    10_10_not_found_422

--- src/main/java/ru/javawebinar/topjava/util/exception/NotFoundException.java
+++ src/main/java/ru/javawebinar/topjava/util/exception/NotFoundException.java
@@ -1,5 +1,10 @@
 package ru.javawebinar.topjava.util.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+//  http://stackoverflow.com/a/22358422/548473
+@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY, reason = "No data found")  // 422
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
