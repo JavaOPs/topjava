5051734a8486fec8dc1ab6abe479a70764028eb8
kaklyugin <kaklyugin@anc.com>
2024-04-14 12:15:17 +0300

    10_05_persist_validate_group

--- src/main/java/ru/javawebinar/topjava/View.java
+++ src/main/java/ru/javawebinar/topjava/View.java
@@ -1,10 +1,7 @@
 package ru.javawebinar.topjava;
 
-public class View {
-    public interface JsonREST {}
-    public interface JsonUI {}
+import javax.validation.groups.Default;
 
-    // https://narmo7.wordpress.com/2014/04/26/how-to-set-up-validation-group-in-springmvc/
-    // http://forum.spring.io/forum/spring-projects/web/117289-validated-s-given-groups-should-consider-default-group-or-not
-    public interface ValidatedUI {}
+public class View {
+    public interface Persist extends Default {}
 }
