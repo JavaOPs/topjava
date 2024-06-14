82e12308b3dcaf2059f9738e496af4cf7cde9ae5
egor <egormalov.2007@gmail.com>
2024-06-14 13:35:37 +1000

    2_0_fix_TimeUtil

--- src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
+++
@@ -1,9 +0,0 @@
-package ru.javawebinar.topjava.util;
-
-import java.time.LocalTime;
-
-public class TimeUtil {
-    public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return !lt.isBefore(startTime) && lt.isBefore(endTime);
-    }
-}
