e9a67c9218b3956679a5dd1984021929dedad474
Oleg Chumakob <krepkiyOleshek@mail.ru>
2024-07-31 23:31:26 +0300

    test commit 2

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.format("Hello TopJava Enterprise!");
         System.out.println("testGitHubCommit");
+        System.out.println("TestcommittoHM0 branch");
     }
 }
