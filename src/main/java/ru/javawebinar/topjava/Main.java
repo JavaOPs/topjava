ade580e60eaba3f541f5105356fda24e00480476
Meiir Akhmetov <meir.akhmetov@gmail.com>
2024-11-25 10:01:20 +0500

    added var test to Main.java

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -8,7 +8,7 @@ package ru.javawebinar.topjava;
  * @link https://github.com/JavaOPs/topjava
  */
 public class Main {
-    private String TestTestMaster
+    private String testMaster;
     public static void main(String[] args) {
         System.out.format("Hello Topjava Enterprise!");
     }
